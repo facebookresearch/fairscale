249f0e54576fe9be81fe9e252955da35890ad88e
Tom Birch <tbirch@fb.com>
2020-07-22 14:49:12 -0700

    Add FB license, update NOTICE with NVIDIA license

--- fairscale/nn/model_parallel/random.py
+++ fairscale/nn/model_parallel/random.py
@@ -1,4 +1,10 @@
 # coding=utf-8
+
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
+
 # Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
