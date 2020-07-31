4f0db9f02716b0ec691ea8fcf79e307360cf70a5
Benjamin Lefaudeux <blefaudeux@users.noreply.github.com>
2020-07-30 19:26:49 -0700

    Revert "formatting/lint again, setting up vscode"
    This reverts commit ff4b30a369e20a041fadd10acf35feb01e4fa1f8.

--- fairscale/optim/utils.py
+++ fairscale/optim/utils.py
@@ -4,7 +4,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import io
-from typing import Any, Dict
+from typing import Any, Dict, Tuple
 
 import torch
 from torch._six import container_abcs
