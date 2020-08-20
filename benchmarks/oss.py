b212deeeedf40a95c214e9e9df1e6ba6145af4e2
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2020-08-20 09:50:32 -0700

    Some cleanup, distributing the training on two GPUs

--- benchmarks/oss.py
+++ benchmarks/oss.py
@@ -2,6 +2,7 @@
 
 
 import os
+import time
 from typing import Any, List
 
 import torch
@@ -9,11 +10,11 @@ import torch.distributed as dist
 import torch.multiprocessing as mp
 import torch.nn as nn
 from torch.utils.data import DataLoader
+
+from fairscale.optim.oss import OSS
 from torchvision.datasets import FakeData
 from torchvision.models import resnet50
 from torchvision.transforms import ToTensor
-
-from fairscale.optim.oss import OSS
 
 BACKEND = dist.Backend.NCCL if torch.cuda.is_available() else dist.Backend.GLOO  # type: ignore
 
@@ -24,34 +25,40 @@ def dist_init(rank, world_size):
     dist.init_process_group(backend=BACKEND, rank=rank, world_size=world_size)
 
 
-def train(
-    rank: int, world_size: int, num_epochs: int = 10, batch_size: int = 32, device: torch.device = torch.device("cuda")
-):
+def train(rank: int, world_size: int, num_epochs: int = 10, batch_size: int = 32):
     # DDP
     dist_init(rank, world_size)
 
     # Standard RN50
-    model = resnet50(pretrained=False, progress=True)
+    model = resnet50(pretrained=False, progress=True).to(rank)
     print("Benchmarking model: ", model)
 
     # Data setup, dummy data
     def collate(inputs: List[Any]):
         return {
-            "inputs": torch.stack([i[0] for i in inputs]).to(device),
-            "label": torch.stack([i[1] for i in inputs]).to(device),
+            "inputs": torch.stack([i[0] for i in inputs]).to(rank),
+            "label": torch.stack([i[1] for i in inputs]).to(rank),
         }
 
-    dataloader = DataLoader(dataset=FakeData(transform=ToTensor(), size=200), batch_size=batch_size, collate_fn=collate)
+    def _print(msg):
+        if dist.get_rank() == 0:
+            print(msg)
+
+    num_images = 200
+    dataloader = DataLoader(
+        dataset=FakeData(transform=ToTensor(), size=num_images), batch_size=batch_size, collate_fn=collate
+    )
     loss_fn = nn.CrossEntropyLoss()
 
     # Shard the optimizer
     optimizer = OSS(params=model.parameters(), optim=torch.optim.SGD, lr=1e-4)
 
     # Dummy training loop
-    model.to(device)
     model.train()
     for epoch in range(num_epochs):
-        print(f"[{dist.get_rank()}] : Epoch {epoch}")
+        _print(f"\n[{dist.get_rank()}] : Epoch {epoch}")
+        epoch_start = time.monotonic()
+
         for batch in dataloader:
 
             def closure():
@@ -61,10 +68,14 @@ def train(
                 dist.all_reduce(loss, op=dist.ReduceOp.SUM)
                 loss /= world_size
                 loss.backward()
-                print(f"[{dist.get_rank()}] : loss {loss.item()}")
+                _print(f"[{dist.get_rank()}] : loss {loss.item():.2f}")
                 return loss
 
             optimizer.step(closure)
+
+        epoch_end = time.monotonic()
+        img_per_sec = num_images / (epoch_end - epoch_start)
+        _print(f"[{dist.get_rank()}] : processed {img_per_sec:.2f} img per sec")
 
 
 if __name__ == "__main__":
