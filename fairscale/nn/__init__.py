22ff665dcf6914ec77a3a5d906284510bea8e9be
msbaines <35972327+msbaines@users.noreply.github.com>
2020-10-08 13:00:22 -0700

    [feat] moe: initial implementation of MOELayer (#128)
    Currently only implemented for a single process and expert.

--- fairscale/nn/__init__.py
+++ fairscale/nn/__init__.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .moe import Top2Gate
+from .moe import MOELayer, Top2Gate
 from .pipe import Pipe
 
 __all__ = ["Pipe", "Top2Gate"]
