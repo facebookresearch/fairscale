a89e0d56b5a72814a72d53ade967261581a3060e
Mandeep Singh Baines <mandeep.baines@gmail.com>
2020-10-16 14:08:38 -0700

    [cleanup] moe: rename moelayer.py to moe_layer.py

--- fairscale/nn/moe/__init__.py
+++ fairscale/nn/moe/__init__.py
@@ -3,5 +3,5 @@
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .moelayer import MOELayer
+from .moe_layer import MOELayer
 from .top2gate import Top2Gate
