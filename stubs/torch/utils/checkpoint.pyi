66b2b5145ef9799f64e8056858c43609ec936bea
Min Xu <24926999+min-xu-ai@users.noreply.github.com>
2020-10-20 11:24:23 -0700

    [test] fine tune test for checkpoint & DDP (#148)
    - fixed typing
    - make it run less often to reduce CI time
    
    testing: run it in a loop make sure it is run in the right frequency.

--- stubs/torch/utils/checkpoint.pyi
+++ stubs/torch/utils/checkpoint.pyi
@@ -2,5 +2,7 @@
 
 from typing import Tuple
 from .. import Tensor
+from torch.nn.modules.module import Module
 
 def detach_variable(inputs: Tuple[Tensor,...]) -> Tuple[Tensor,...]: ...
+def checkpoint(function: Module, *args, **kwargs): ...
