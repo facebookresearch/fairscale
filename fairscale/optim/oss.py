9f5c55d9e648dccb547385a6b97d1ba215a8bc86
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2020-11-04 14:22:56 -0800

    adding an hopefully better explanation as of why this is useful

--- fairscale/optim/oss.py
+++ fairscale/optim/oss.py
@@ -217,7 +217,8 @@ class OSS(Optimizer):
             Total norm of the parameters (viewed as a single vector).
 
         .. note: This is analogous to `torch.nn.utils.clip_grad_norm_` but handles the partitioning and multiple devices per rank
-        under the hood.
+        under the hood. The default torch util is not applicaple here, because each rank only has a partial view of all the grads
+        in the model, so calling it in the OSS context would lead to different scaling being applied per subset of model parameters
 
         .. warning: This needs to be called on all ranks, since synchronization primitives will be used
         .. warning: Model paralelism -groups other than world- are not yet supported
