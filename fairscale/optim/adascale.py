cff932bc30c96c3548a7e0a09e74d7ff975f74d7
Min Xu <m1n@fb.com>
2020-11-20 09:52:11 -0800

    [adascale] added gradient accumulation
    - added gradient accumulation
    - tested with cifar full trainings with different value of accumulation
    and verified the full accuracy is obtained
    - also removed the patch optimize flag until we need it

--- fairscale/optim/adascale.py
+++ fairscale/optim/adascale.py
@@ -70,9 +70,10 @@ class AdaScale(object):
             Scaling factor of the batch size, e.g. using a 10x
             larger batch size (summed across all world_size) means a scale of
             10. If None, defaults to ``world_size``.
-        patch_optimizer (bool):
-            If True, monkey-patches the ``step`` method of
-            the optimizer with the AdaScale's ``step`` method.
+        smoothing (float):
+            Smoothing factor between batches. Default value: 0.9999
+        num_gradients_to_accumulate (int):
+            Number of passes that we accumulate gradients locally.
     """
 
     def __init__(
@@ -81,16 +82,25 @@ class AdaScale(object):
         world_size: Optional[int] = None,
         scale: Optional[float] = None,
         smoothing: float = 0.999,
-        patch_optimizer: bool = False,
+        num_gradients_to_accumulate: int = 1,
     ):
         self._optimizer = optimizer
-        self._optimizer_step = optimizer.step
         self._local_grad_sqr: Optional[torch.Tensor] = None
-        self._world_size: int = (world_size if world_size is not None else torch.distributed.get_world_size())
+        self._world_size: int = (
+            world_size
+            if world_size is not None
+            else torch.distributed.get_world_size()
+            if torch.distributed.is_initialized()
+            else 1
+        )
+        self._smoothing = smoothing
+        self._num_backward_calls = 0
+        self._num_grads_to_accum = num_gradients_to_accumulate
 
-        if self._world_size <= 1:
-            raise RuntimeError("AdaScale does not support a single worker.")
+        if self._world_size * self._num_grads_to_accum <= 1:
+            raise RuntimeError("AdaScale does not support a single worker without grad accumulation.")
 
+        # Per-param-group sqr & var states (sigma^2 & mu^2 in the paper).
         self._optimizer.state.setdefault(
             "adascale",
             {
@@ -99,19 +109,19 @@ class AdaScale(object):
             },
         )
 
-        self.set_scale(self._world_size if scale is None else scale)
+        self.set_scale(self._world_size * self._num_grads_to_accum if scale is None else scale)
 
+        # Register the gradient hooks. Note, don't assume every param will generate
+        # a gradient (i.e. triggering the hook) in every backward pass.
         for idx, param_group in enumerate(self._optimizer.param_groups):
             for param in param_group["params"]:
                 param.register_hook(functools.partial(self._backward_hook, idx))
 
-        if patch_optimizer:
-            self.patch_optimizer()
-
-        self._smoothing = smoothing
-
     @property
     def state(self) -> Dict[str, np.ndarray]:
+        """
+        Return the states of AdaScale.
+        """
         return self._optimizer.state["adascale"]
 
     @property
@@ -121,6 +131,10 @@ class AdaScale(object):
         batch size when training with a single worker. For example, if the
         baseline batch size is 32, but using a scaled-up batch size of 80, then
         then the scaling factor is 2.5.
+
+        Returns:
+            (float):
+                The current scaling factor.
         """
         return self._scale
 
@@ -136,46 +150,64 @@ class AdaScale(object):
         """
         self._scale = scale
 
-    def grad_sqr_avg(self) -> float:
+    def grad_sqr_avg(self, pg_idx: Optional[int] = None) -> float:
         """
-        Current estimate of the squared l2-norm of the true gradient (sigma
-        squared in the AdaScale paper).
+        Current estimate of the squared l2-norm of the true gradient
+        (sigma squared in the AdaScale paper).
 
-        Returns
+        Args:
+            pg_idx (Optional[int]):
+                Optional index for a parameter group.
+
+        Returns:
             (float):
                 Estimate of squared l2-norm.
         """
-        return np.sum(self.state["grad_sqr_avg"])
+        if pg_idx is not None:
+            return self.state["grad_sqr_avg"][pg_idx]
+        else:
+            return np.sum(self.state["grad_sqr_avg"])
 
-    def grad_var_avg(self) -> float:
+    def grad_var_avg(self, pg_idx: Optional[int] = None) -> float:
         """
         Current estimate of the trace of the covariance of the true gradient
         (mu squared in the AdaScale paper).
 
-        Returns
+        Args:
+            pg_idx (Optional[int]):
+                Optional index for a parameter group.
+
+        Returns:
             (float):
                 Estimate of trace of the covariance.
         """
-        return np.sum(self.state["grad_var_avg"])
+        if pg_idx is not None:
+            return self.state["grad_var_avg"][pg_idx]
+        else:
+            return np.sum(self.state["grad_var_avg"])
 
-    def gain(self, scale: Optional[float] = None) -> float:
+    def gain(self, scale: Optional[float] = None, pg_idx: Optional[int] = None) -> float:
         """
-        Current estimate of the AdaScale gain ratio (r_t).
+        Current estimate of the AdaScale gain ratio (r_t in the paper).
 
         Args:
             scale (float):
-                The batch size scale to estimate the gain ratio for.
+                Optional batch size scale to estimate the gain ratio for.
+            pg_idx (int):
+                Optional index of a parameter group.
 
-        Returns
-            :(float):
+        Returns:
+            (float):
                 Estimate of gain ratio.
         """
         scale = self._scale if scale is None else scale
-        var = self.grad_var_avg()
-        sqr = self.grad_sqr_avg()
+        var = self.grad_var_avg(pg_idx)
+        sqr = self.grad_sqr_avg(pg_idx)
         return (var + sqr) / (var / scale + sqr)
 
-    def _update_avg(self, name: str, value: float, factor: float) -> None:
+    def _update_avg(self, name: str, value: torch.Tensor, factor: float) -> None:
+        # This function computes and stores the moving average of a vector
+        # using a smoothing factor.
         biased = self.state.get(name + "_biased", 0.0)
         unbias = self.state.get(name + "_unbias", 0.0)
         biased = factor * biased + (1.0 - factor) * value
@@ -184,12 +216,18 @@ class AdaScale(object):
         self.state[name + "_unbias"] = unbias
         self.state[name] = biased / unbias
 
-    def _backward_hook(self, idx: int, grad: torch.Tensor) -> None:
+    def _backward_hook(self, pg_idx: int, grad: torch.Tensor) -> None:
         # This method should be invoked once for each parameter during the
         # backward pass, before gradients are synchronized between world_size.
+
+        # Store the local gradient square sums in a vector.
         if self._local_grad_sqr is None:
             self._local_grad_sqr = torch.zeros(len(self._optimizer.param_groups), device=grad.device)
-        self._local_grad_sqr[idx] += grad.pow(2).sum()
+        self._local_grad_sqr[pg_idx] += grad.pow(2).sum()
+
+        # Now, ensure we queue a callback at the end of the callback queue.
+        # This will fire after all gradient callbacks are done (esp. those
+        # queued by DDP.
         self._final_callback_queued = False
         Variable._execution_engine.queue_callback(self._queue_callback)
 
@@ -208,26 +246,51 @@ class AdaScale(object):
 
     def _final_callback(self) -> None:
         # This method should be invoked once for each backward pass, after
-        # gradients have been synchronized between each worker.
+        # gradients have been synchronized between each worker, unless we
+        # are in gradient accumulation mode, where grads are not all_reduced
+        # between the GPUs.
         self._final_callback_queued = False
         assert isinstance(self._local_grad_sqr, torch.Tensor)
 
-        # self._local_grad_sqr is FP32, sum then div shouldn't overflow.
-        torch.distributed.all_reduce(self._local_grad_sqr)  # SUM
-        self._local_grad_sqr.div_(self._world_size)
+        # Keep track of number of backward calls for gradient accumulation.
+        self._num_backward_calls += 1
 
-        local_grad_sqr = self._local_grad_sqr.cpu().numpy()
+        if self._num_backward_calls % self._num_grads_to_accum != 0:
+            return
+
+        # Since self._local_grad_sqr is FP32, sum shouldn't overflow.
+        # This vector has length of # of param_groups, so it is small, but we
+        # use async to hide the all_reduce latency, esp when # of nodes is large.
+        work = torch.distributed.all_reduce(self._local_grad_sqr, async_op=True)  # SUM
+
+        # Compute the sums of squares for reduced gradients.
+        # Divide by _num_grads_to_accum since the gradients are accumulated.
+        #
+        # Note: we are mutating the gradients here!!!
         total_grad_sqr = np.array(
-            [sum(param.grad.pow(2).sum().item() for param in group["params"]) for group in self._optimizer.param_groups]
+            [
+                sum(param.grad.div_(self._num_grads_to_accum).pow(2).sum().item() for param in group["params"])
+                for group in self._optimizer.param_groups
+            ]
         )
-        grad_sqr = (self._world_size * total_grad_sqr - local_grad_sqr) / (self._world_size - 1)
-        grad_var = (local_grad_sqr - total_grad_sqr) * self._scale / (self._world_size - 1)
-        grad_sqr = np.maximum(grad_sqr, 0.0)
+
+        # Wait for all_reduce to be done and move it to cpu & np.
+        work.wait()
+        local_grad_sqr = self._local_grad_sqr.cpu().numpy()
+        self._local_grad_sqr = None
+
+        # See appendix B.3 of the paper.
+        #
+        # local_grad_sqr is \sigma_{i=1}^{S}\norm{g_t_i}
+        # total_grad_sqr is \norm{g_t}
+        S = self._scale
+        grad_var = local_grad_sqr / (S - 1) - total_grad_sqr * S / (S - 1)
+        grad_sqr = total_grad_sqr - grad_var / S
         grad_var = np.maximum(grad_var, 1e-6)
-        theta = self._smoothing ** self._scale
+        grad_sqr = np.maximum(grad_sqr, 0.0)
+        theta = self._smoothing ** S
         self._update_avg("grad_sqr_avg", grad_sqr, theta)
         self._update_avg("grad_var_avg", grad_var, theta)
-        self._local_grad_sqr = None
 
     def step(self, *args: Any, **kwargs: Any) -> Optional[float]:
         """
@@ -235,35 +298,29 @@ class AdaScale(object):
         ``optimizer.step(*args, **kwargs)`` with a scaled learning rate.
 
         Args:
-            args:
+            args (Any):
                 Positional arguments passed to ``optimizer.step``.
-            kwargs:
+            kwargs (Any):
                 Keyword arguments passed to ``optimizer.step``.
+
         Returns:
             (Tensor):
-                loss if a closure is passed to the optimizer to reevaluate the model.
+                The loss tensor if a closure if used to re-evaluate the model.
         """
-        initial_lr = [pg["lr"] for pg in self._optimizer.param_groups]
+        # Set original LR and set new LR.
+        original_lr = []
         for idx, param_group in enumerate(self._optimizer.param_groups):
-            grad_sqr = float(self.state["grad_sqr_avg"][idx])
-            grad_var = float(self.state["grad_var_avg"][idx])
-            gain = (grad_var + grad_sqr) / (grad_var / self._scale + grad_sqr)
-            param_group["lr"] = gain * param_group["lr"]
-        res = self._optimizer_step(*args, **kwargs)
-        for lr, param_group in zip(initial_lr, self._optimizer.param_groups):
+            original_lr.append(param_group["lr"])
+            param_group["lr"] = self.gain(pg_idx=idx) * param_group["lr"]
+
+        # Step it.
+        res = self._optimizer.step(*args, **kwargs)
+
+        # Restore the original LR.
+        for lr, param_group in zip(original_lr, self._optimizer.param_groups):
             param_group["lr"] = lr
+
         return res
-
-    def patch_optimizer(self) -> None:
-        """
-        Monkey-patch the optimizer's step function with :meth:`AdaScale.step`.
-        """
-
-        @functools.wraps(self._optimizer.step)
-        def wrapper(*args: Any, **kwargs: Any) -> Optional[float]:
-            return self.step(*args, **kwargs)
-
-        setattr(self._optimizer, "step", wrapper)
 
     def zero_grad(self) -> None:
         """Proxy function to optimizer"""
