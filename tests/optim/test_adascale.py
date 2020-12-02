ef53d18318d40e741c2f22d144142347f6f639e0
Min Xu <min.xu@acm.org>
2020-12-01 17:11:56 -0800

    added ddp grad_accum test

--- tests/optim/test_adascale.py
+++ tests/optim/test_adascale.py
@@ -28,10 +28,6 @@ skip_if_single_gpu = pytest.mark.skipif(torch.cuda.device_count() < 2, reason="m
 def test_basic_cpu():
     """Test single batch behavior on CPU"""
     model = Linear(2, 2, bias=False)
-    model.weight.data[0, 0] = 0.1
-    model.weight.data[0, 1] = 0.2
-    model.weight.data[1, 0] = 0.3
-    model.weight.data[1, 1] = 0.4
     try:
         optim = AdaScale(SGD(model.parameters(), lr=0.1))
     except RuntimeError:
@@ -45,11 +41,7 @@ def test_loss_accum_cpu():
     Loss accumulation is NOT SUPPORTED. This test shows that it does not work.
     """
     model = Linear(2, 2, bias=False)
-    model.weight.data[0, 0] = 0.1
-    model.weight.data[0, 1] = 0.2
-    model.weight.data[1, 0] = 0.3
-    model.weight.data[1, 1] = 0.4
-    # num_gradients_to_accumulate value doesn't matter
+    # num_gradients_to_accumulate value doesn't matter in this negative test.
     optim = AdaScale(SGD(model.parameters(), lr=0.1), num_gradients_to_accumulate=123)
     # data 1
     in_data = Tensor([0.0, 1.0])
@@ -62,20 +54,16 @@ def test_loss_accum_cpu():
     loss += model(in_data).sum()
     # backward, but gradient is only produced once by the autograd engine.
     loss.backward()
-    # therefore, the gain will always be 1, which render adascale as noop.
+    # therefore, the gain will always be 1, which renders adascale as noop.
     optim.step()
     assert np.allclose(optim.gain(), 1.0), optim.gain()
 
 
 def test_grad_accum_cpu():
-    """Test the basic functionality on CPU"""
+    """Test the basic functionality on CPU with gradient accumulation without DDP"""
     model = Linear(2, 2, bias=False)
-    model.weight.data[0, 0] = 0.1
-    model.weight.data[0, 1] = 0.2
-    model.weight.data[1, 0] = 0.3
-    model.weight.data[1, 1] = 0.4
     optim = AdaScale(SGD(model.parameters(), lr=0.1), num_gradients_to_accumulate=2)
-    for expected_gain in [2.0, 2.0]:  # do 2 iterations
+    for expected_gain in [2.0, 2.0]:  # test 2 iterations catch more corner cases.
         # grad pass 1
         in_data = Tensor([0.0, 1.0])
         out = model(in_data)
@@ -84,8 +72,9 @@ def test_grad_accum_cpu():
         in_data = Tensor([1.0, 0.0])
         out = model(in_data)
         out.sum().backward()
-        # stepping it. Note that if we did more than 2 passes, AdaScale won't be able to
-        # check it for now. The result will just be wrong if that's the case.
+        # stepping it. Note that if we did more than 2 passes as promised by the
+        # num_gradients_to_accumulate argument above, AdaScale is not be able to
+        # detect that mistake for now. The result will just be wrong in that case.
         assert np.allclose(optim.gain(), expected_gain), optim.gain()
         optim.step()
         optim.zero_grad()
@@ -106,13 +95,9 @@ def _dist_init(rank, world_size, tempfile_name, backend):
 
 
 def _test_ddp_func(rank, world_size, tempfile_name):
-    _dist_init(rank, world_size, tempfile_name, backend="nccl")
+    _dist_init(rank, world_size, tempfile_name, backend="nccl")  # Covers nccl
 
     model = Linear(2, 2, bias=False)
-    model.weight.data[0, 0] = 0.1
-    model.weight.data[0, 1] = 0.2
-    model.weight.data[1, 0] = 0.3
-    model.weight.data[1, 1] = 0.4
     model.to("cuda")
     model = DDP(model, device_ids=[rank])
     optim = AdaScale(SGD(model.parameters(), lr=0.1))
@@ -131,13 +116,45 @@ def _test_ddp_func(rank, world_size, tempfile_name):
 
 @skip_if_single_gpu
 def test_ddp():
+    """Test adascale with DDP without gradient accumulation"""
     world_size = 2
     temp_file_name = tempfile.mkstemp()[1]
 
     mp.spawn(_test_ddp_func, args=(world_size, temp_file_name), nprocs=world_size, join=True)
 
 
+def _test_ddp_grad_accum_func(rank, world_size, tempfile_name):
+    _dist_init(rank, world_size, tempfile_name, backend="gloo")  # Covers gloo
+
+    model = Linear(4, 2, bias=False)
+    model.to("cuda")
+    model = DDP(model, device_ids=[rank])
+    optim = AdaScale(SGD(model.parameters(), lr=0.1), num_gradients_to_accumulate=2)
+    with model.no_sync():
+        # iter 1, input vectors are pointing dim0 and dim1
+        in_data = Tensor([0.0] * 4)
+        in_data[rank] = 1.0
+        in_data = in_data.cuda()
+        out = model(in_data)
+        out.sum().backward()
+    # iter 2, input vectors are pointing dim2 and dim3
+    in_data = Tensor([0.0] * 4)
+    in_data[rank + 2] = 1.0
+    in_data = in_data.cuda()
+    out = model(in_data)
+    out.sum().backward()
+    # since all inputs are orthogonal, the gain should be exactly 4.0.
+    assert np.allclose(optim.gain(), 4.0), optim.gain()
+    optim.step()
+    optim.zero_grad()
+
+    dist.destroy_process_group()
+
+
 @skip_if_single_gpu
 def test_grad_accum_ddp():
-    # TODO: test like ddp but with model.no_sync()
-    pass
+    """Test adascale with DDP + gradient accumulation using ddp.no_sync()"""
+    world_size = 2
+    temp_file_name = tempfile.mkstemp()[1]
+
+    mp.spawn(_test_ddp_grad_accum_func, args=(world_size, temp_file_name), nprocs=world_size, join=True)
