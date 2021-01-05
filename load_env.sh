8157603d465ec90f5f263c93e0b1572d8f618869
Vinayak Tantia <tantia.vinayak1@gmail.com>
2021-01-05 13:24:52 -0800

    Add scripts to load environment and format code

---
+++ load_env.sh
@@ -0,0 +1,7 @@
+module purge
+module load cuda/10.1
+module load NCCL/2.7.6-1-cuda.10.1
+module load cudnn/v7.6.5.32-cuda.10.1
+module load anaconda3/2020.11  # Might need to change this to change Python version
+
+source ~/.venv/fairscale_venv/bin/activate
