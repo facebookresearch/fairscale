3a0957617dd525be0d2ab115643c16c42a13d430
Vinayak Tantia <tantia.vinayak1@gmail.com>
2021-01-05 16:04:04 -0800

    Remove remaining adpsgd code

--- fairscale/nn/data_parallel/gossip/__init__.py
+++ fairscale/nn/data_parallel/gossip/__init__.py
@@ -5,7 +5,6 @@
 # LICENSE file in the root directory of this source tree.
 #
 
-from .ad_psgd import BilatGossipDataParallel
 from .distributed import GossipDataParallel
 from .gossiper import PushPull, PushSum
 from .graph_manager import (
