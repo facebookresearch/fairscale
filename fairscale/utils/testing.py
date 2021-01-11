4e45192f094dd40821c8ca4e9235ceecf95febc5
Benjamin Lefaudeux <benjamin.lefaudeux@protonmail.com>
2021-01-10 22:15:48 -0800

    mypy shenanigans, mp stub

--- fairscale/utils/testing.py
+++ fairscale/utils/testing.py
@@ -156,7 +156,7 @@ def spawn_for_all_world_sizes(test_func: Callable, world_sizes: List[int] = get_
         _, filename_rpc = tempfile.mkstemp()
 
         # (lefaudeux) Let mp handle the process joining, join=False and handling context has been unstable in the past
-        mp.spawn(test_func, args=(world_size, filename, filename_rpc, *args), nprocs=world_size, join=True)  # noqa
+        mp.spawn(test_func, args=(world_size, filename, filename_rpc, *args), nprocs=world_size, join=True)
 
 
 def worker_process(
