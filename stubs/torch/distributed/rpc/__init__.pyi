6af400e66b2e53fb977046d9b070c119887966c9
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2021-01-11 22:34:55 +0000

    stubs

--- stubs/torch/distributed/rpc/__init__.pyi
+++ stubs/torch/distributed/rpc/__init__.pyi
@@ -3,40 +3,34 @@
 from typing import Union, Callable, Optional, Any
 from torch.futures import Future
 
-
-class RRef:
-    ...
-
-
-class WorkerInfo:
-    ...
+class RRef: ...
+class WorkerInfo: ...
 
 class BackendType:
     TENSORPIPE: Any
     PROCESS_GROUP: Any
 
-def TensorPipeRpcBackendOptions(init_method: str) -> Any : ...
-
+def TensorPipeRpcBackendOptions(init_method: str) -> Any: ...
+def ProcessGroupRpcBackendOptions(init_method: str) -> Any: ...
 def rpc_async(
     to: Union[str, WorkerInfo],
     func: Callable,
     args: Optional[tuple] = None,
     kwargs: Optional[dict] = None,
     timeout=-1.0,
-) -> Future:
-    ...
-
-
+) -> Future: ...
 def rpc_sync(
     to: Union[str, WorkerInfo],
     func: Callable,
     args: Optional[tuple] = None,
     kwargs: Optional[dict] = None,
     timeout=-1.0,
-) -> None:
-    ...
-
-
-def init_rpc(name: str, backend: Optional[Any] = None, rank:int = -1, world_size: Optional[int] = None, rpc_backend_options: Optional[Any] = None) -> None: ...
-
+) -> None: ...
+def init_rpc(
+    name: str,
+    backend: Optional[Any] = None,
+    rank: int = -1,
+    world_size: Optional[int] = None,
+    rpc_backend_options: Optional[Any] = None,
+) -> None: ...
 def shutdown() -> None: ...
