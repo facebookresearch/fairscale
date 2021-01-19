44b9bcd811cb7f38980a265f53342bee2b602507
anj-s <32556631+anj-s@users.noreply.github.com>
2021-01-19 14:10:30 -0800

    [refactor] Enable benchmarks/pipe.py and merge real and synthetic input pipeline. (#286)
    * [refactor]Remove unused variables and refactor common configurations
    
    * move helper function to call site
    
    * fixed lint errors
    
    * fix lint errors
    
    * fix lint errors
    
    * fix lint errors
    
    * fix import order
    
    * format files
    
    * remove unused imports
    
    * fix lint errors
    
    * fix lint errors
    
    * refactor common utilities
    
    * address PR comments
    
    * sorted imports
    
    * add space
    
    * modify comment
    
    * added doc strings and addressed PR comments.
    
    * addressed PR comments
    
    * added another comment to clarify.
    
    * fixing lint errors
    
    * addressed PR comments
    
    * addressed PR comments
    
    * fixed typos
    
    * initialize var
    
    * rename seq_pred to lm
    
    * fix lint errors
    
    * move datasets and models into separate folders
    
    * add the folders created
    
    * fix lint errors
    
    * create golden config to stats mapping
    
    * add common batching for both synthetic and real data
    
    * fixed lint errors
    
    * enable real pipe benchmakrs with new golden data
    
    * reduce seq len to avoid OOM
    
    * updated golden data
    
    * add logging
    
    * add golden data
    
    * add golden data
    
    * fix lint errors
    
    * add doc string
    
    * remove commented out line
    
    * address comments
    
    * rename imports
    
    * refactor common logic in dataloaders
    
    * add golden configs
    
    * lint changes
    
    Co-authored-by: Anjali Sridhar <anj@devfair0443.h2.fair>

---
+++ benchmarks/datasets/wikitext2_data.py
@@ -0,0 +1,69 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+
+import io
+
+import torch
+from torch.utils.data import DataLoader
+from torchtext.data.utils import get_tokenizer
+from torchtext.utils import download_from_url, extract_archive
+from torchtext.vocab import build_vocab_from_iterator
+
+
+def _batchify(data, batch_size):
+    data = torch.tensor(data)
+    # Divide the dataset into bsz parts.
+    nbatch = data.size(0) // batch_size
+    # Trim off any extra elements that wouldn't cleanly fit (remainders).
+    data = data.narrow(0, 0, nbatch * batch_size)
+    # Evenly divide the data across the bsz batches.
+    data = data.view(batch_size, -1).t().contiguous()
+    return data
+
+
+def get_real_dataloaders(args):
+    """Return real dataloaders for training, testing and validation."""
+
+    url = "https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-v1.zip"
+    test_filepath, valid_filepath, train_filepath = extract_archive(download_from_url(url, root="/tmp"))
+    tokenizer = get_tokenizer("basic_english")
+
+    def data_process(raw_text_iter):
+        data = [torch.tensor([vocab[token] for token in tokenizer(item)], dtype=torch.long) for item in raw_text_iter]
+        return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
+
+    vocab = build_vocab_from_iterator(map(tokenizer, iter(io.open(train_filepath, encoding="utf8"))))
+
+    train_dataset = data_process(iter(io.open(train_filepath, encoding="utf8")))
+    valid_dataset = data_process(iter(io.open(valid_filepath, encoding="utf8")))
+    test_dataset = data_process(iter(io.open(test_filepath, encoding="utf8")))
+
+    def batchify(data):
+        batch_size = args.batch_size
+        return _batchify(data, batch_size)
+
+    # TODO(anj-s): Both seq_len and batch size should be part of the golden config.
+    seq_len = 32
+    total_batch_size = seq_len * args.batch_size
+    train_dataloader = DataLoader(train_dataset, batch_size=total_batch_size, collate_fn=batchify)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=total_batch_size, collate_fn=batchify)
+    test_dataloader = DataLoader(test_dataset, batch_size=total_batch_size, collate_fn=batchify)
+    return len(vocab.stoi), train_dataloader, valid_dataloader, test_dataloader
+
+
+def get_synthetic_dataloaders(args):
+    """Return synthetic dataloaders for training, testing and validation."""
+
+    def batchify(data):
+        batch_size = args.batch_size
+        return _batchify(data, batch_size)
+
+    # TODO(anj-s): Both seq_len and batch size should be part of the golden config.
+    seq_len = 32
+    total_batch_size = seq_len * args.batch_size
+    # vocab_size is 10000 and length of the real data is 2049990.
+    lm_dataset = torch.randint(1, 10000, (2049990,))
+
+    lm_dataloader = DataLoader(
+        lm_dataset, batch_size=total_batch_size, shuffle=True, num_workers=0, collate_fn=batchify
+    )
+    return lm_dataloader, lm_dataloader, lm_dataloader
