42889e670fbfb9c1a99a0164caccc995fcb7e894
Min Xu <min.xu@acm.org>
2021-02-18 15:23:31 -0800

    update copyright year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 # -- Project information -----------------------------------------------------
 
 project = "FairScale"
-copyright = "2020, Facebook AI Research"
+copyright = "2020-2021, Facebook AI Research"
 author = "Facebook AI Research"
 
 # The full version, including alpha/beta/rc tags
