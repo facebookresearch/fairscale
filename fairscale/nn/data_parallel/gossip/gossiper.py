4fd8be9d2dbeac52e89d5d75fad1869c10292e58
Vinayak Tantia <tantia.vinayak1@gmail.com>
2021-02-19 18:42:46 -0800

    Change license header on all the files

--- fairscale/nn/data_parallel/gossip/gossiper.py
+++ fairscale/nn/data_parallel/gossip/gossiper.py
@@ -1,9 +1,7 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-# All rights reserved.
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
 #
-# This source code is licensed under the license found in the
+# This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
-#
 
 """
 Gossipers
