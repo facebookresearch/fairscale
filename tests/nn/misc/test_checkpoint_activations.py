157ec24ec4abca92e6d6d760040aa79d69f7d75e
Min Xu <min.xu@acm.org>
2021-02-22 20:42:14 -0800

    more debugging

--- tests/nn/misc/test_checkpoint_activations.py
+++ tests/nn/misc/test_checkpoint_activations.py
@@ -5,11 +5,10 @@
 
 """Test fairscale.nn.misc.checkpoint_activations API."""
 
-import unittest
-
+import pytest
 import torch
 import torch.nn as nn
-from torch.utils.checkpoint import checkpoint as torch_checkpoint
+from torch.utils.checkpoint import checkpoint as torch_checkpoint_wrapper
 
 from fairscale.nn.misc.checkpoint_activations import checkpoint_wrapper
 
@@ -22,7 +21,38 @@ def get_cuda_mem_allocated():
         return 0
 
 
-class Model(nn.Module):
+def get_loss_and_gnorm(model, input):
+    """Helper to run a forward/backward pass and return results in a dict."""
+    ret = {}
+
+    ret["mem_0"] = get_cuda_mem_allocated()
+    ret["mem_peak"] = 0
+    if ret["mem_0"] > 0:
+        torch.cuda.reset_peak_memory_stats()
+
+    model.zero_grad()
+    loss = model(input).sum()
+    ret["mem_after_fwd"] = get_cuda_mem_allocated()
+
+    loss.backward()
+    ret["mem_after_bwd"] = get_cuda_mem_allocated()
+
+    gnorm = torch.norm(torch.stack([torch.norm(p.grad.detach()) for p in model.parameters()]))
+    ret["loss"] = loss.item()
+    ret["gnorm"] = gnorm.item()
+
+    if ret["mem_0"] > 0:
+        ret["mem_peak"] = torch.cuda.max_memory_allocated()
+
+    return ret
+
+
+class BasicModel(nn.Module):
+    """Basic model with a single FFN being checkpointed.
+
+       Used for extensive checkings: equivalency with non-checkpoint, torch-checkpoint, etc.
+    """
+
     def __init__(self, use_pytorch_checkpoint=False, use_fairscale_checkpoint=False, **kwargs):
         super().__init__()
         torch.manual_seed(0)  # make sure weights are deterministic.
@@ -42,89 +72,97 @@ class Model(nn.Module):
 
     def forward(self, x):
         if self.use_pytorch_checkpoint:
-            x = torch_checkpoint(self.ffn, x)
+            x = torch_checkpoint_wrapper(self.ffn, x)
         else:
             x = self.ffn(x)
         return self.out(x)
 
 
-class TestComparisonToPyTorch(unittest.TestCase):
-    def _test_checkpoint_wrapper(self, device):
-        def get_loss_and_gnorm(model, input):
-            ret = {}
+class CpuOffloadModel(nn.Module):
+    """Model used to check cpu offload memory saving"""
 
-            ret["mem_0"] = get_cuda_mem_allocated()
-            ret["mem_peak"] = 0
-            if ret["mem_0"] > 0:
-                torch.cuda.reset_peak_memory_stats()
+    def __init__(self, enable_checkpoint=False, cpu_offload=False):
+        super().__init__()
+        torch.manual_seed(0)  # make sure weights are deterministic.
+        self.layers = nn.Sequential(
+            nn.Sequential(nn.Linear(2, 4), nn.Linear(4, 8), nn.Linear(8, 10)),
+            nn.Sequential(nn.Linear(10, 32), nn.Linear(32, 64), nn.Linear(64, 1)),
+        )
+        if enable_checkpoint:
+            for i, layer in enumerate(self.layers):
+                self.layers[i] = checkpoint_wrapper(layer, cpu_offload)
 
-            model.zero_grad()
-            loss = model(input).sum()
-            ret["mem_after_fwd"] = get_cuda_mem_allocated()
-
-            loss.backward()
-            ret["mem_after_bwd"] = get_cuda_mem_allocated()
-
-            gnorm = torch.norm(torch.stack([torch.norm(p.grad.detach()) for p in model.parameters()]))
-            ret["loss"] = loss.item()
-            ret["gnorm"] = gnorm.item()
-
-            if ret["mem_0"] > 0:
-                ret["mem_peak"] = torch.cuda.max_memory_allocated()
-
-            return ret
-
-        input = torch.rand(2, 16, 32).requires_grad_(True)
-        model = Model().to(device)
-        no_cpt = get_loss_and_gnorm(model, input.to(device))
-
-        model = Model(use_pytorch_checkpoint=True).to(device)
-        pyt_cpt = get_loss_and_gnorm(model, input.to(device))
-
-        model = Model(use_fairscale_checkpoint=True).to(device)
-        fairscale_cpt = get_loss_and_gnorm(model, input.to(device))
-
-        model = Model(use_fairscale_checkpoint=True, offload_to_cpu=True).to(device)
-        fairscale_cpt_offload = get_loss_and_gnorm(model, input.to(device))
-
-        # Check for correctness.
-        torch.testing.assert_allclose(no_cpt["loss"], pyt_cpt["loss"])
-        torch.testing.assert_allclose(no_cpt["gnorm"], pyt_cpt["gnorm"])
-
-        torch.testing.assert_allclose(no_cpt["loss"], fairscale_cpt["loss"])
-        torch.testing.assert_allclose(no_cpt["gnorm"], fairscale_cpt["gnorm"])
-
-        torch.testing.assert_allclose(no_cpt["loss"], fairscale_cpt_offload["loss"])
-        torch.testing.assert_allclose(no_cpt["gnorm"], fairscale_cpt_offload["gnorm"])
-
-        # Check for memory usage for cuda only.
-        if device == torch.device("cpu"):
-            return
-        for d in [no_cpt, pyt_cpt, fairscale_cpt, fairscale_cpt_offload]:
-            del d["loss"]
-            del d["gnorm"]
-        assert no_cpt == {"mem_0": 38912, "mem_peak": 98816, "mem_after_fwd": 64000, "mem_after_bwd": 74240}, no_cpt
-        assert pyt_cpt == {"mem_0": 38912, "mem_peak": 103424, "mem_after_fwd": 43520, "mem_after_bwd": 74240}, pyt_cpt
-        assert fairscale_cpt == {
-            "mem_0": 38912,
-            "mem_peak": 103424,
-            "mem_after_fwd": 43520,
-            "mem_after_bwd": 74240,
-        }, fairscale_cpt
-        assert fairscale_cpt_offload == {
-            "mem_0": 38912,
-            "mem_peak": 107520,
-            "mem_after_fwd": 43520,
-            "mem_after_bwd": 74240,
-        }, fairscale_cpt_offload
-
-    def test_checkpoint_wrapper_cpu(self):
-        self._test_checkpoint_wrapper(device=torch.device("cpu"))
-
-    @unittest.skipIf(not torch.cuda.is_available(), "test requires a GPU")
-    def test_checkpoint_wrapper_cuda(self):
-        self._test_checkpoint_wrapper(device=torch.device("cuda"))
+    def forward(self, x):
+        return self.layers(x)
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.parametrize("device", ["cpu", "cuda"])
+def test_basic(device):
+    if "cuda" in device and not torch.cuda.is_available():
+        pytest.skip("test requires a GPU")
+
+    input = torch.rand(2, 16, 32).requires_grad_(True)
+    model = BasicModel().to(device)
+    no_cpt = get_loss_and_gnorm(model, input.to(device))
+
+    model = BasicModel(use_pytorch_checkpoint=True).to(device)
+    pyt_cpt = get_loss_and_gnorm(model, input.to(device))
+
+    model = BasicModel(use_fairscale_checkpoint=True).to(device)
+    fairscale_cpt = get_loss_and_gnorm(model, input.to(device))
+
+    model = BasicModel(use_fairscale_checkpoint=True, offload_to_cpu=True).to(device)
+    fairscale_cpt_offload = get_loss_and_gnorm(model, input.to(device))
+
+    # Check for correctness.
+    torch.testing.assert_allclose(no_cpt["loss"], pyt_cpt["loss"])
+    torch.testing.assert_allclose(no_cpt["gnorm"], pyt_cpt["gnorm"])
+
+    torch.testing.assert_allclose(no_cpt["loss"], fairscale_cpt["loss"])
+    torch.testing.assert_allclose(no_cpt["gnorm"], fairscale_cpt["gnorm"])
+
+    torch.testing.assert_allclose(no_cpt["loss"], fairscale_cpt_offload["loss"])
+    torch.testing.assert_allclose(no_cpt["gnorm"], fairscale_cpt_offload["gnorm"])
+
+    # Check for memory usage for cuda only.
+    if "cpu" in device:
+        return
+    for d in [no_cpt, pyt_cpt, fairscale_cpt, fairscale_cpt_offload]:
+        del d["loss"]
+        del d["gnorm"]
+        print("XXX", d["mem_peak"])
+
+    assert no_cpt == {"mem_0": 38912, "mem_peak": 98816, "mem_after_fwd": 64000, "mem_after_bwd": 74240}, no_cpt
+    assert pyt_cpt == {"mem_0": 38912, "mem_peak": 103424, "mem_after_fwd": 43520, "mem_after_bwd": 74240}, pyt_cpt
+    assert fairscale_cpt == {
+        "mem_0": 38912,
+        "mem_peak": 103424,
+        "mem_after_fwd": 43520,
+        "mem_after_bwd": 74240,
+    }, fairscale_cpt
+    assert fairscale_cpt_offload == {
+        "mem_0": 38912,
+        "mem_peak": 107520,
+        "mem_after_fwd": 43520,
+        "mem_after_bwd": 74240,
+    }, fairscale_cpt_offload
+
+
+def test_offload_memory():
+    device = "cuda"
+    if "cuda" in device and not torch.cuda.is_available():
+        pytest.skip("test requires a GPU")
+
+    input = torch.rand(60, 24, 2).requires_grad_(True)
+
+    model = CpuOffloadModel().to(device)
+    base = get_loss_and_gnorm(model, input.to(device))
+
+    model = CpuOffloadModel(True).to(device)
+    cpt = get_loss_and_gnorm(model, input.to(device))
+
+    model = CpuOffloadModel(True, True).to(device)
+    offload = get_loss_and_gnorm(model, input.to(device))
+
+    print(base, cpt, offload)
+    assert 0
