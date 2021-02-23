2b15720bb00e1b3502b22313c500f51c5e14397a
Min Xu <24926999+min-xu-ai@users.noreply.github.com>
2021-02-22 18:30:48 -0800

    [docs] fsdp changelog and doc (#414)

--- fairscale/nn/__init__.py
+++ fairscale/nn/__init__.py
@@ -3,13 +3,14 @@
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .data_parallel import ShardedDataParallel
+from .data_parallel import FullyShardedDataParallel, ShardedDataParallel
 from .misc import FlattenParamsWrapper
 from .moe import MOELayer, Top2Gate
 from .pipe import Pipe, PipeRPCWrapper
 
 __all__ = [
     "FlattenParamsWrapper",
+    "FullyShardedDataParallel",
     "LazyModule",
     "Pipe",
     "PipeRPCWrapper",
