9b419abb6c2ffab17f93eab27147cc70d8928c46
Min Xu <m1n@fb.com>
2021-02-23 15:06:47 -0800

    comment

--- fairscale/nn/misc/checkpoint_activations.py
+++ fairscale/nn/misc/checkpoint_activations.py
@@ -66,6 +66,9 @@ def checkpoint_wrapper(module: nn.Module, offload_to_cpu: bool = False) -> nn.Mo
     # The use of weakref here is to prevent creating a ref cycle: m -> m.forward -> m.
     # When such cycle exists, gc won't collect the module when the module is freed.
     # That causes GPU memory is leaked. See the unit test for how we catch that.
+    #
+    # We prefer this over a class wrapper since the class wrapper would have to
+    # proxy a lot of fields and methods.
     module.forward = functools.partial(_checkpointed_forward, type(module).forward, weakref.ref(module), offload_to_cpu)  # type: ignore
     return module
 
