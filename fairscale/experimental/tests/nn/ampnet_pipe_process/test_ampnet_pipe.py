3f879097b70257cf67659af4bbe5b481110160f6
Anjali Sridhar <anj@devfair0443.h2.fair>
2021-02-23 17:43:51 -0800

    lint error fix

--- fairscale/experimental/tests/nn/ampnet_pipe_process/test_ampnet_pipe.py
+++ fairscale/experimental/tests/nn/ampnet_pipe_process/test_ampnet_pipe.py
@@ -17,11 +17,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pytest
 import torch
 from torch import nn
 from torch.optim.optimizer import Optimizer
 from torch.utils.data import DataLoader, Dataset
-import pytest
+
 from fairscale.experimental.nn.ampnet_pipe.pipe import AMPnetPipe
 from fairscale.utils.testing import get_worker_map, torch_spawn
 
