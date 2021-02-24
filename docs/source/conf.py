322645447da7ea61549efc3935ae453d8cbdbad3
Myle Ott <myleott@fb.com>
2021-02-24 07:51:09 -0800

    Fix tests?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,11 @@ release = "0.3.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosectionlabel"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.napoleon",  # support NumPy and Google style docstrings
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
