d2a354c4fc4e2817aaff5e343d8105e3a77ec7d6
Min Xu <min.xu@acm.org>
2021-02-27 13:19:28 -0800

    add test list files

---
+++ tests/ci_test_list_check.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Verify that we don't miss any tests.
+
+find_total=`find tests -name \*.py -type f| sort | uniq | wc -l`
+grep_total=`grep py tests/ci_test_list*.txt | sort | uniq | wc -l`
+
+echo $find_total
+echo $grep_total
+
+if [ $find_total != $grep_total ]; then
+   echo "unit test is missing from CI"
+   exit 1
+fi
