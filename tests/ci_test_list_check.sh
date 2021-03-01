664677fa167535f04f93d7808ee24e39090aa017
Min Xu <min.xu@acm.org>
2021-03-01 13:48:39 -0800

    add a test file, improved the checking script

--- tests/ci_test_list_check.sh
+++ tests/ci_test_list_check.sh
@@ -2,13 +2,11 @@
 
 # Verify that we don't miss any tests.
 
-find_total=`find tests -name \*.py -type f| grep -v __init__.py | sort | uniq | wc -l`
-grep_total=`grep py tests/ci_test_list*.txt | sort | uniq | wc -l`
+find tests -name \*.py -type f| grep -v __init__.py | sort | uniq > /tmp/find.out
+cat tests/ci_test_list*.txt | sort | uniq > /tmp/cat.out
 
-echo $find_total
-echo $grep_total
-
-if [ $find_total != $grep_total ]; then
-   echo "unit test is missing from CI"
+if ! diff /tmp/find.out /tmp/cat.out ; then
+   echo "Unit test is missing from CI"
+   echo "See the diff above to fix it"
    exit 1
 fi
