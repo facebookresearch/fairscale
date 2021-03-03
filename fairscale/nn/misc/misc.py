2aa6c8289610d6de59987bcf63f016cb0dd24dcb
Min Xu <min.xu@acm.org>
2021-03-03 13:30:06 -0800

    fix

--- fairscale/nn/misc/misc.py
+++ fairscale/nn/misc/misc.py
@@ -3,11 +3,14 @@
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
+from typing import Tuple
+
+import torch
 from torch import nn
 from torch.nn.modules.batchnorm import _BatchNorm
 
 
-def patch_batchnorm(module: nn.Module) -> None:
+def patch_batchnorm(module: nn.Module) -> Tuple:
     """Patch all batchnorm instances (1d, 2d, 3d, sync_bn, etc.) of a module
        so that they don't track running stats when torch.no_grad() is enabled.
 
@@ -19,22 +22,28 @@ def patch_batchnorm(module: nn.Module) -> None:
     Args:
         module (nn.Module):
             The module to be patched in-place.
+
+    Returns:
+        (tuple):
+            A list of hook handles, late can be freed.
     """
-    # Build a list of BN classes and use two functions only make them track
-    # running stats in one of the forward function.
-    bn_list = []
+    hooks = []
+    def pre_forward(module, input) -> None:
+        if torch.is_grad_enabled():
+            return
+        module._track_running_stats_backup = module.track_running_stats
+        module.track_running_stats = False
+
+    def post_forward(module, input, result) -> None:
+        if torch.is_grad_enabled():
+            return
+        module.track_running_stats = module._track_running_stats_backup
+
     for name, child in module.named_modules():
         # _BatchNorm is base for bn1d, bn2d, bn3d and sync_bn, apex_sync_bn, etc.
         if isinstance(child, _BatchNorm):
-            bn_list.append(child)
-
-    def pre_forward() -> None:
-        for bn in bn_list:
-            bn._track_running_stats_backup = bn.track_running_stats
-            bn.track_running_stats = False
-
-    def post_forward() -> None:
-        for bn in bn_list:
-            bn.track_running_stats = bn._track_running_stats_backup
-
-    # Register the pre/post hooks.
+            # Register the pre/post hooks.
+            pre_handle = child.register_forward_pre_hook(pre_forward)
+            post_handle = child.register_forward_hook(post_forward)
+            hooks += [pre_handle, post_handle]
+    return hooks
