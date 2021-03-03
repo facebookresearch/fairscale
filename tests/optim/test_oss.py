dc8210fc4119f181d827b881611167c4aee33e42
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2021-03-03 01:43:52 +0000

    change empty shard handling for OSS, do not rely on asserts

--- tests/optim/test_oss.py
+++ tests/optim/test_oss.py
@@ -262,11 +262,14 @@ def test_zero_grad():
     mp.spawn(run_test_zero_grad, args=(world_size, temp_file_name), nprocs=world_size, join=True)
 
 
-def run_test_catch_empty_shardd(rank, world_size, tempfile_name):
+def run_test_empty_shardd(rank, world_size, tempfile_name):
     dist_init(rank, world_size, tempfile_name, backend="gloo")
     m = torch.nn.Linear(1, 1)
-    with pytest.raises(AssertionError):
-        _ = optim.OSS(m.parameters(), lr=0.1)
+    o = optim.OSS(m.parameters(), lr=0.1)
+    x = torch.rand(20, 1)
+    y = m(x).sum()
+    y.backward()
+    o.step()
 
     dist.destroy_process_group()
 
@@ -274,7 +277,7 @@ def run_test_catch_empty_shardd(rank, world_size, tempfile_name):
 def test_empty_shard():
     world_size = 4
 
-    mp.spawn(run_test_catch_empty_shardd, args=(world_size, tempfile.mkstemp()[1]), nprocs=world_size, join=True)
+    mp.spawn(run_test_empty_shardd, args=(world_size, tempfile.mkstemp()[1]), nprocs=world_size, join=True)
 
 
 def run_test_step(rank, world_size, tempfile_name):
