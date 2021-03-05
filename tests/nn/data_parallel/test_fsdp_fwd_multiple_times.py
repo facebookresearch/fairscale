85afe8b7e44febc46ef2e26308e86e20abac92c2
Min Xu <min.xu@acm.org>
2021-03-05 14:16:15 -0800

    refactored the test

--- tests/nn/data_parallel/test_fsdp_fwd_multiple_times.py
+++ tests/nn/data_parallel/test_fsdp_fwd_multiple_times.py
@@ -14,81 +14,106 @@ import tempfile
 import pytest
 import torch
 import torch.multiprocessing as mp
-from torch.nn import Linear, Module
+from torch.nn import Linear, Module, Sequential
 from torch.optim import Adam
 
 from fairscale.nn.data_parallel import FullyShardedDataParallel as FSDP
 from fairscale.nn.data_parallel.fully_sharded_data_parallel import TrainingState
-from fairscale.nn.misc import checkpoint_wrapper
+from fairscale.nn.misc import checkpoint_wrapper as ckpt
+from fairscale.nn.wrap import enable_wrap, wrap
 from fairscale.utils.testing import dist_init, skip_if_single_gpu, teardown, torch_version
 
+# To make it simpler to write and easier to understand, we do
+# FSDP and checkpointing in the Model classes explicitly.
+#
+# This means the models are created at the workers not by the
+# controller/main process.
+#
+# For this test, we don't do any numerical checking, so the weights
+# of the models at the each worker doesn't matter and we don't make
+# them the same.
 
-class MyModel(Module):
-    """Model that forward multiple times through the same module."""
 
-    def __init__(self, params_in_root):
+class ModelCkpt1Level(Module):
+    """ Model that has one level checkpoint."""
+
+    def __init__(self):
         super().__init__()
-        self.params_in_root = params_in_root
-        self.child1 = Linear(3, 3, bias=False)
-        self.child2 = Linear(3, 3, bias=False)
-        if self.params_in_root:
-            self.layer_in_root_fsdp = Linear(3, 3, bias=False)
+        self.child1 = ckpt(Linear(3, 3, bias=False))
+        self.child2 = ckpt(Linear(3, 3, bias=False))
 
     def forward(self, x):
-        # Same layers, once.
         x = self.child1(x)
         x = self.child2(x)
-        # Twice.
-        x = self.child1(x)
-        x = self.child2(x)
-        # Optionally, params in root FSDP.
-        if self.params_in_root:
-            x = self.layer_in_root_fsdp(x)
         return x
 
 
-def _test_func(rank, world_size, model, fsdp_config, checkpoint, tempfile_name, unused):
+class ModelCkpt2Level(Module):
+    """ Model that has two level checkpoint."""
+
+    def __init__(self):
+        super().__init__()
+        self.child1 = ckpt(Sequential(ckpt(Linear(3, 3, bias=False)), ckpt(Linear(3, 3, bias=False))))
+        self.child2 = ckpt(Sequential(ckpt(Linear(3, 3, bias=False)), ckpt(Linear(3, 3, bias=False))))
+
+    def forward(self, x):
+        x = self.child1(x)
+        x = self.child2(x)
+        return x
+
+
+class ModelFSDP1Level(Module):
+    """ Model that has one level checkpoint."""
+
+    def __init__(self):
+        super().__init__()
+        self.child1 = wrap(Linear(3, 3, bias=False))
+        self.child2 = wrap(Linear(3, 3, bias=False))
+
+    def forward(self, x):
+        x = self.child1(x)
+        x = self.child2(x)
+        return x
+
+
+# List of all Models.
+MODELS = [ModelCkpt1Level, ModelCkpt2Level, ModelFSDP1Level]
+
+
+def _test_func(rank, world_size, model_cls, fsdp_config, tempfile_name, unused):
     result = dist_init(rank, world_size, tempfile_name, unused)
     assert result, "Dist init failed"
 
-    model.cuda()
     assert isinstance(fsdp_config, dict), str(fsdp_config)
-    assert checkpoint in [True, False], checkpoint
+    fsdp_config["wrapper_cls"] = FSDP
+    with enable_wrap(**fsdp_config):
+        model = model_cls().cuda()
 
-    # Inner FSDPs.
-    model.child1 = FSDP(model.child1, **fsdp_config)
-    model.child2 = FSDP(model.child2, **fsdp_config)
-    # Optionally checkpoint them.
-    if checkpoint:
-        model.child1 = checkpoint_wrapper(model.child1)
-        model.child2 = checkpoint_wrapper(model.child2)
-    # Root FSDP.
-    model = FSDP(model, **fsdp_config)
-    # Use Adam to increase coverage
+    # Use Adam to increase overall coverage among all FSDP tests.
     optim = Adam(model.parameters(), lr=5e-4)
 
     for _ in range(5):
         in_data = torch.rand(64, 3).cuda()
         in_data.requires_grad = True
         out = model(in_data)
-        # without double fwd, no_params_in_root assert on backward
-        # since we have multiple checkpointed blocks.
         out.sum().backward()
         optim.step()
         optim.zero_grad()
 
-    model.assert_state(TrainingState.IDLE)
+    if hasattr(model, "assert_state"):
+        model.assert_state(TrainingState.IDLE)
     teardown()
 
 
 @skip_if_single_gpu
 @pytest.mark.parametrize(
-    "fsdp_config", [{}, {"flatten_parameters": False}],
+    # Sweep both flatten and not flatten since it likely matters here.
+    "fsdp_config",
+    [{}, {"flatten_parameters": False}],
 )
-@pytest.mark.parametrize("checkpoint", [True, False])
-@pytest.mark.parametrize("params_in_root", [True, False])
-def test_it(fsdp_config, checkpoint, params_in_root):
-    """Test FSDP with uneven divide of parameter shards."""
+@pytest.mark.parametrize("model_cls", MODELS)
+def test_it(fsdp_config, model_cls):
+    """Test FSDP and checkpoint_wrapper with different model configs."""
     if torch_version() < (1, 6, 0):
         pytest.skip("older pytorch doesn't support reduce_scatter")
 
@@ -97,11 +122,6 @@ def test_it(fsdp_config, checkpoint, params_in_root):
     temp_file_name = tempfile.mkstemp()[1]
     unused = tempfile.mkstemp()[1]
 
-    model = MyModel(params_in_root)
-
     mp.spawn(
-        _test_func,
-        args=(world_size, model, fsdp_config, checkpoint, temp_file_name, unused),
-        nprocs=world_size,
-        join=True,
+        _test_func, args=(world_size, model_cls, fsdp_config, temp_file_name, unused), nprocs=world_size, join=True,
     )
