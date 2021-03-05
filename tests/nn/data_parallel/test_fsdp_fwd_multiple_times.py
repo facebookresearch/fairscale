37f64d7143525ccc8228e2bc13b71c57b4649b29
Min Xu <min.xu@acm.org>
2021-03-04 19:43:44 -0800

    one known failure case without fwd twice

--- tests/nn/data_parallel/test_fsdp_fwd_multiple_times.py
+++ tests/nn/data_parallel/test_fsdp_fwd_multiple_times.py
@@ -26,14 +26,22 @@ from fairscale.utils.testing import dist_init, skip_if_single_gpu, teardown, tor
 class MyModel(Module):
     """Model that forward multiple times through the same module."""
 
-    def __init__(self):
+    def __init__(self, params_in_root):
         super().__init__()
-        self.layer = Linear(3, 3, bias=False)
+        self.params_in_root = params_in_root
+        self.child1 = Linear(3, 3, bias=False)
+        self.child2 = Linear(3, 3, bias=False)
+        if self.params_in_root:
+            self.layer_in_root_fsdp = Linear(3, 3, bias=False)
 
     def forward(self, x):
         # Same layer, multiple times.
-        x = self.layer(x)
-        x = self.layer(x)
+        x = self.child1(x)
+        # x = self.child1(x)
+        x = self.child2(x)
+        # x = self.child2(x)
+        if self.params_in_root:
+            x = self.layer_in_root_fsdp(x)
         return x
 
 
@@ -45,16 +53,24 @@ def _test_func(rank, world_size, model, fsdp_config, checkpoint, tempfile_name, 
     assert isinstance(fsdp_config, dict), str(fsdp_config)
     assert checkpoint in [True, False], checkpoint
 
-    model.layer = FSDP(model.layer, **fsdp_config)
+    # Inner FSDPs.
+    model.child1 = FSDP(model.child1, **fsdp_config)
+    model.child2 = FSDP(model.child2, **fsdp_config)
+    # Optionally checkpoint them.
     if checkpoint:
-        model.layer = checkpoint_wrapper(model.layer)
+        model.child1 = checkpoint_wrapper(model.child1)
+        model.child2 = checkpoint_wrapper(model.child2)
+    # Root FSDP.
     model = FSDP(model, **fsdp_config)
+    # Use Adam to increase coverage
     optim = Adam(model.parameters(), lr=5e-4)
 
     for _ in range(5):
         in_data = torch.rand(64, 3).cuda()
         in_data.requires_grad = True
         out = model(in_data)
+        # without double fwd, no_params_in_root assert on backward
+        # since we have multiple checkpointed blocks.
         out.sum().backward()
         optim.step()
         optim.zero_grad()
@@ -68,7 +84,8 @@ def _test_func(rank, world_size, model, fsdp_config, checkpoint, tempfile_name, 
     "fsdp_config", [{}, {"flatten_parameters": False}],
 )
 @pytest.mark.parametrize("checkpoint", [True, False])
-def test_it(fsdp_config, checkpoint):
+@pytest.mark.parametrize("params_in_root", [True, False])
+def test_it(fsdp_config, checkpoint, params_in_root):
     """Test FSDP with uneven divide of parameter shards."""
     if torch_version() < (1, 6, 0):
         pytest.skip("older pytorch doesn't support reduce_scatter")
@@ -78,7 +95,7 @@ def test_it(fsdp_config, checkpoint):
     temp_file_name = tempfile.mkstemp()[1]
     unused = tempfile.mkstemp()[1]
 
-    model = MyModel()
+    model = MyModel(params_in_root)
 
     mp.spawn(
         _test_func,
