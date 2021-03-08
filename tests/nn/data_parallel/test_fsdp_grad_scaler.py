[fix]: handle inputs with containers in mixed precision (#486)
* [fix]: handle inputs with containers

- this is an issue surfaces by vissl as well
- fix seems to be super simple
- also cleaned up two tests with respect to multiple such tests
  running back to back (they don't do that presently)

* cleanup

* fix

* lint

tests/nn/data_parallel/test_fsdp_grad_scaler.py
@@ -1,5 +1,16 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+""" Test FSDP with grad scaler. """
+
 import os
-from unittest import mock
+import random
 
 import pytest
 import torch
@@ -17,28 +28,36 @@ except ImportError:
     pytestmark = pytest.mark.skip
 
 
-@mock.patch.dict(os.environ, {"MASTER_ADDR": "localhost", "MASTER_PORT": "1337"}, clear=True)
+# Mixed precision needs cuda.
 @skip_if_no_cuda
 def test_scaler_cpu_offload_breaks():
-
     device = torch.device("cuda")
     torch.cuda.set_device(0)
 
+    # Random port in case the next test run quickly, same port would cause conflict.
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = str(random.randint(2000, 3000))
     torch.distributed.init_process_group(backend="nccl", rank=0, world_size=1)
 
-    scaler = ShardedGradScaler()
-    model = FullyShardedDataParallel(nn.Linear(5, 5), cpu_offload=True, mixed_precision=True)
-    optim = torch.optim.SGD(model.parameters(), lr=1e-3)
+    try:
+        scaler = ShardedGradScaler()
+        model = FullyShardedDataParallel(nn.Linear(5, 5), cpu_offload=True, mixed_precision=True)
+        optim = torch.optim.SGD(model.parameters(), lr=1e-3)
 
-    input = torch.rand((1, 5), dtype=torch.float).to(device)
-    optim.zero_grad()
-    with autocast():
-        output = model(input)
-        loss = F.mse_loss(input, output)
+        input = torch.rand((1, 5), dtype=torch.float).to(device)
+        optim.zero_grad()
+        with autocast():
+            output = model(input)
+            loss = F.mse_loss(input, output)
 
-    scaler.scale(loss).backward()
-    # TODO (Min): Need to fix. Details in issue #421.
-    with pytest.raises(RuntimeError):
-        scaler.step(optim)
-        scaler.update()
-    torch.distributed.destroy_process_group()
+        scaler.scale(loss).backward()
+        # TODO (Min): Need to fix. Details in issue #421.
+        with pytest.raises(RuntimeError):
+            scaler.step(optim)
+            scaler.update()
+
+    finally:
+        # Clean-up is important or the next test in this file may fail to init the PG.
+        torch.distributed.destroy_process_group()
+        del os.environ["MASTER_ADDR"]
+        del os.environ["MASTER_PORT"]