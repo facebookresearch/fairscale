886aa32730618dbcac35edc8dbdbb69e826ef6bf
Benjamin Lefaudeux <blefaudeux@users.noreply.github.com>
2021-03-08 11:10:13 -0800

    [chore] OSS perf test, super minor (#495)

--- benchmarks/golden_configs/oss_mnist.py
+++ benchmarks/golden_configs/oss_mnist.py
@@ -4,7 +4,7 @@
 def get_golden_real_stats():
 
     return {
-        "reference_speed": 650,
+        "reference_speed": 660,
         "reference_memory": 1000,
         "reference_loss": 0.026,
     }
