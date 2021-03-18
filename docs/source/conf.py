8cacf33278e06556e72fa008f079a77b5fef0354
Min Xu <m1n@fb.com>
2021-03-18 10:48:48 -0700

    [chore] 0.3.2 release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ copyright = "2020-2021, Facebook AI Research"
 author = "Facebook AI Research"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.1"
+release = "0.3.2"
 
 
 # -- General configuration ---------------------------------------------------
