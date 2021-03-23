Fix broadcast_object regression

fairscale/nn/data_parallel/fully_sharded_data_parallel.py
@@ -21,7 +21,7 @@ import torch.nn.functional as F
 
 from fairscale.nn.misc import FlattenParamsWrapper
 from fairscale.nn.wrap import auto_wrap, default_auto_wrap_policy, enable_wrap
-from fairscale.optim.utils import calc_grad_norm, recursive_copy_to_device
+from fairscale.optim.utils import broadcast_object, calc_grad_norm, recursive_copy_to_device
 from fairscale.utils.containers import apply_to_tensors
 from fairscale.utils.parallel import chunk_and_pad, enable_pytorch_sync_bn, validate_process_group
 from fairscale.utils.reduce_scatter_bucketer import ReduceScatterBucketer
@@ -1369,17 +1369,17 @@ class FullyShardedDataParallel(nn.Module):
         # Store all the states in order, rank by rank
         should_collect_state = recipient_rank is None or (self.rank == recipient_rank)
         all_states: List[Dict[str, Any]] = []
+        dummy_tensor = torch.tensor([0], dtype=torch.uint8, device=self.compute_device)
         for rank in range(self.world_size):
             if rank == self.rank:
                 sd = optim.state_dict()
                 sd["num_padded"] = [m.num_padded for m in self._fsdp_instances]
             else:
-                sd = None  # type: ignore
-            obj_lst = [sd]
-            torch.distributed.broadcast_object_list(obj_lst, src=rank, group=self.process_group)
+                sd = dummy_tensor  # type: ignore
+            sd = broadcast_object(sd, src_rank=rank, group=self.process_group, dist_device=self.compute_device)  # type: ignore
             if should_collect_state:
-                assert isinstance(obj_lst[0], dict), f"{rank}, {self.rank} {all_states}"
-                all_states.append(recursive_copy_to_device(obj_lst[0], non_blocking=False, device=torch.device("cpu")))
+                assert isinstance(sd, dict), f"{self.rank} received {type(sd)} from {rank}, expected dict"
+                all_states.append(recursive_copy_to_device(sd, non_blocking=False, device=torch.device("cpu")))
 
         return all_states
 

tests/nn/data_parallel/test_fsdp_optimizer_utils.py
@@ -1,5 +1,4 @@
 import functools
-import unittest
 
 from parameterized import parameterized
 import torch
@@ -30,9 +29,12 @@ def assert_equal(a, b):
     assert a == b, f"{a} != {b}"
 
 
+from time import time
+
+
 class TestOptimizerUtils(DistributedTest):
     @parameterized.expand(
-        [[functools.partial(SGD, momentum=0.9), True], [SGD, False], [Adam, False],],
+        [[functools.partial(SGD, momentum=0.9), True], [SGD, False], [Adam, False], [Adadelta, True]],
         name_func=rename_test,
     )
     def test_consolidate_optimizer(self, optim_fn, transformer):
@@ -41,7 +43,7 @@ class TestOptimizerUtils(DistributedTest):
             self._test_consolidated_optimizer, config, optim_fn=optim_fn, transformer=transformer
         )
 
-        spawn_and_init(test_fn, world_sizes=[1, min(torch.cuda.device_count(), 4)])
+        spawn_and_init(test_fn, world_sizes=[min(torch.cuda.device_count(), 4)])
 
     @classmethod
     def _test_consolidated_optimizer(self, config, rank, group, optim_fn=torch.optim.SGD, transformer=False):
@@ -78,7 +80,12 @@ class TestOptimizerUtils(DistributedTest):
         unwrapped_sd = optim_unwrapped.state_dict()
 
         # first_key = unwrapped_sd['state'][0].keys()
+        tstart = time()
         sd = fsdp.gather_full_optim_state_dict(fsdp_optim, recipient_rank=0)
+        duration = time() - tstart
+        # Switching from fairscale.optim.utils.broadcast_object to torch.broadcast_object_list will cause this to raise
+        assert duration < fsdp.world_size, f"gather optim state took {duration} seconds, suspect change in _consolidate"
+
         if fsdp.rank > 0:
             return
 