daa1bad5d3f536c4880eea2b2f4af84291b2976c
Benjamin Lefaudeux <blefaudeux@users.noreply.github.com>
2021-03-30 13:36:59 -0700

    [feat][fix] ShardedDDP deferred init (#558)
    * survive the model being moved to device post-construction
    * make sure that a unit test would catch a regression

--- fairscale/nn/misc/param_bucket.py
+++ fairscale/nn/misc/param_bucket.py
@@ -73,8 +73,12 @@ class ParamBucket(Bucket):
     @torch.no_grad()
     def _add_param_as_view(self, param: torch.Tensor, keep_existing_value: bool = True) -> None:
         assert self.buffer is not None
-        assert param.dtype == self.buffer.dtype
-        assert param.device == self.buffer.device
+        assert (
+            param.dtype == self.buffer.dtype
+        ), f"Different types for the bucket and the param, cannot proceed: {param.dtype} - {self.buffer.dtype}"
+        assert (
+            param.device == self.buffer.device
+        ), f"Different devices for the bucket and the param, cannot proceed: {param.device} - {self.buffer.device}"
 
         fill_next = self._fill + param.numel()
         assert fill_next <= self.buffer.numel()
