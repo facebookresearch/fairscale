9fd8b696c8bf96c38b6d9911634ee223948c275b
Min Xu <min.xu@acm.org>
2021-04-23 14:46:35 -0700

    enable all 8 test cases

--- tests/nn/data_parallel/test_fsdp_multiple_forward_checkpoint.py
+++ tests/nn/data_parallel/test_fsdp_multiple_forward_checkpoint.py
@@ -12,6 +12,7 @@
 import contextlib
 import pickle
 
+import pytest
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
@@ -22,7 +23,8 @@ import torch.optim as optim
 from fairscale.nn import checkpoint_wrapper
 from fairscale.nn.data_parallel import FullyShardedDataParallel as FSDP
 from fairscale.nn.data_parallel import auto_wrap_bn
-from fairscale.utils.testing import dist_init, skip_if_single_gpu, teardown, temp_files_ctx
+from fairscale.nn.wrap import enable_wrap, wrap
+from fairscale.utils.testing import dist_init, objects_are_equal, skip_if_single_gpu, teardown, temp_files_ctx
 
 
 class Model(nn.Module):
@@ -46,25 +48,33 @@ class Model(nn.Module):
             return torch.cat(ys, dim=0)
 
 
-def create_model(with_fsdp, with_checkpoint):
+def create_model(with_fsdp, with_checkpoint, mixed_precision, flatten, wrap_bn, fp32_reduce_scatter):
     model = Model()
     if with_fsdp:
-        # XXX: test auto_wrap_bn on & off.
-        if True:
+        if wrap_bn:
             model.block1 = auto_wrap_bn(model.block1, single_rank_pg=False)
             model.block2 = auto_wrap_bn(model.block2, single_rank_pg=False)
         if with_checkpoint:
             model.block2 = checkpoint_wrapper(model.block2, maintain_forward_counter=True)
-        model.block1 = FSDP(model.block1)
-        model.block2 = FSDP(model.block2)
-        model.head = FSDP(model.head)
+        with enable_wrap(
+            wrapper_cls=FSDP,
+            flatten_parameters=flatten,
+            mixed_precision=mixed_precision,
+            compute_dtype=torch.float32,
+            fp32_reduce_scatter=fp32_reduce_scatter,
+        ):
+            model.block1 = wrap(model.block1)
+            model.block2 = wrap(model.block2)
+            model.head = wrap(model.head)
     else:
         if with_checkpoint:
             model.block2 = checkpoint_wrapper(model.block2, maintain_forward_counter=False)
     return model
 
 
-def _distributed_worker(gpu_id, world_size, with_fsdp, with_checkpoint, files):
+def _distributed_worker(
+    gpu_id, world_size, with_fsdp, with_checkpoint, files, mixed_precision, flatten, wrap_bn, fp32_reduce_scatter
+):
     filename, filename_rpc = files[:2]
     filename_loss = files[2:]
 
@@ -89,18 +99,31 @@ def _distributed_worker(gpu_id, world_size, with_fsdp, with_checkpoint, files):
         torch.randn(size=(2, 3, 96, 96)).cuda(),
     ]
 
-    model = create_model(with_fsdp, with_checkpoint)
+    if mixed_precision and not with_fsdp:
+        batch = [x.half() for x in batch]
+
+    model = create_model(with_fsdp, with_checkpoint, mixed_precision, flatten, wrap_bn, fp32_reduce_scatter)
     model = model.cuda()
 
     if with_fsdp:
-        model = FSDP(model)
-        context = contextlib.suppress()
+        model = FSDP(
+            model,
+            flatten_parameters=flatten,
+            mixed_precision=mixed_precision,
+            compute_dtype=torch.float32,
+            fp32_reduce_scatter=fp32_reduce_scatter,
+        )
         model.set_gradient_divide_factors(1.0, 2.0, True)
+        no_sync_context = contextlib.suppress()
     else:
         # With DDP, we need no_sync and manual gradient reduction below because
         # it can't handle multiple forward pass + checkpointing otherwise.
         model = DistributedDataParallel(model, device_ids=[gpu_id])
-        context = model.no_sync()
+        no_sync_context = model.no_sync()
+
+    mp_context = contextlib.suppress()
+    if mixed_precision:
+        mp_context = torch.cuda.amp.autocast(enabled=True)
 
     if gpu_id == 0:
         print(model)
@@ -111,19 +134,22 @@ def _distributed_worker(gpu_id, world_size, with_fsdp, with_checkpoint, files):
 
     losses = {}
     i = 0
-    with context:
+    with no_sync_context:
         for iteration in range(3):
-            out = model(batch)
-            loss = criterion(out, target)
-            print("Loss", iteration, ":", loss.item())
-            losses[f"iter_{i}"] = loss.item()
-            i += 1
-            optimizer.zero_grad()
-            loss.backward()
+            with mp_context:
+                out = model(batch)
+                loss = criterion(out, target)
+                print("Loss", iteration, ":", loss.item())
+                losses[f"iter_{i}"] = loss
+                i += 1
+                optimizer.zero_grad()
+                loss.backward()
+            # Manual grad reduction, no autocast.
             if not with_fsdp:
                 for p in model.parameters():
                     dist.all_reduce(p.grad.data)
                     p.grad.data.div_(2.0)
+            # Stepping, no autocast
             optimizer.step()
 
     # Due to dist.all_reduce code block above with ddp.no_sync, we seem to hit a bug
@@ -136,7 +162,15 @@ def _distributed_worker(gpu_id, world_size, with_fsdp, with_checkpoint, files):
 
 
 @skip_if_single_gpu
-def test_multiple_forward_checkpoint():
+@pytest.mark.parametrize("precision", ["full", "mixed"])
+@pytest.mark.parametrize("flatten", ["flatten", "no_flatten"])
+@pytest.mark.parametrize("wrap_bn", ["auto_wrap_bn", "no_auto_wrap_bn"])
+def test_multiple_forward_checkpoint(precision, flatten, wrap_bn):
+    mixed_precision = precision == "mixed"
+    flatten = flatten == "flatten"
+    wrap_bn = wrap_bn == "auto_wrap_bn"
+    fp32_reduce_scatter = True if mixed_precision else None
+
     world_size = 2
     expected_losses = None
     # Ensure ddp == ddp+ckpt == fsdp == fsdp+ckpt.
@@ -144,7 +178,20 @@ def test_multiple_forward_checkpoint():
         for with_checkpoint in [False, True]:
             # Get 4 files: 2 for dist_init and 2 for each rank to save the losses.
             with temp_files_ctx(num=2 + world_size) as temp_files:
-                mp.spawn(_distributed_worker, (world_size, with_fsdp, with_checkpoint, temp_files), nprocs=world_size)
+                mp.spawn(
+                    _distributed_worker,
+                    (
+                        world_size,
+                        with_fsdp,
+                        with_checkpoint,
+                        temp_files,
+                        mixed_precision,
+                        flatten,
+                        wrap_bn,
+                        fp32_reduce_scatter,
+                    ),
+                    nprocs=world_size,
+                )
                 final_losses = {}
                 for rank in range(world_size):
                     with open(temp_files[2 + rank], "rb") as f:
@@ -153,4 +200,4 @@ def test_multiple_forward_checkpoint():
                     expected_losses = final_losses
                 else:
                     print(f"fsdp: {with_fsdp} ckpt: {with_checkpoint}")
-                    assert expected_losses == final_losses
+                    assert objects_are_equal(expected_losses, final_losses, raise_exception=True)
