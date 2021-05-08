520f54cd856e325eef0ff1c0b8f0faf16caf413a
Anjali Sridhar <anj@devfair0443.h2.fair>
2021-05-08 02:32:22 +0000

    add license file headers for all files

--- benchmarks/golden_configs/oss_mnist.py
+++ benchmarks/golden_configs/oss_mnist.py
@@ -1,5 +1,7 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
-
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
 
 def get_golden_real_stats():
 
