13dae922453d98b552b64637c0d58318f7c1c927
Min Xu <min.xu.public@gmail.com>
2021-07-30 14:49:25 -0700

    lint

--- fairscale/nn/data_parallel/fully_sharded_data_parallel.py
+++ fairscale/nn/data_parallel/fully_sharded_data_parallel.py
@@ -1565,7 +1565,7 @@ class FullyShardedDataParallel(nn.Module):
                     p._saved_grad_shard = p.grad.data
                     p.grad = None
                 else:
-                    raise AssertionError(f"unexpected grad shape: {p.grad.size()}"
+                    raise AssertionError(f"unexpected grad shape: {p.grad.size()}")
 
     @torch.no_grad()
     def _free_full_params(self, params: Optional[List[Parameter]] = None) -> None:
