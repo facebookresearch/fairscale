e6f972b94bdbfb89d23bef691c9601a819ab88d8
freewym <freewym@gmail.com>
2021-11-18 05:10:06 -0500

    remove unnecessary import __future__ that causes the "future feature annotations is not defined" error if Python<3.7

--- fairscale/experimental/nn/ssd_offload.py
+++ fairscale/experimental/nn/ssd_offload.py
@@ -3,8 +3,6 @@
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
-from __future__ import annotations
-
 from enum import Enum, auto
 from functools import reduce
 import io
