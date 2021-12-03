4ffaf7b0166a5086a2ced8736b7aa7cfaba8a2ec
Freddy Snijder <freddy@visionscapers.com>
2021-12-03 19:07:09 +0100

    Updated the test to be independent of the Huggingface transformers package

--- tests/nn/data_parallel/test_fsdp_hf_transformer_eval.py
+++ tests/nn/data_parallel/test_fsdp_hf_transformer_eval.py
@@ -1,32 +1,88 @@
+from collections import OrderedDict
+from dataclasses import dataclass
 import tempfile
 import unittest
 
 import torch
+from torch import nn
 
 from fairscale.nn import FullyShardedDataParallel as FSDP
 from fairscale.nn import auto_wrap, enable_wrap
 from fairscale.utils.testing import dist_init
 
-try:
-    from transformers import GPT2Config, GPT2LMHeadModel, GPT2Tokenizer
-    from transformers.models.gpt2.modeling_gpt2 import GPT2Model
-except ImportError:
-    raise unittest.SkipTest(
-        "Test(s) require Huggingface Transformers package installed (pip install transformers), skipping ..."
-    )
 
-
-def wrap_gpt2_block_only(module, recurse, **kwargs):
+def wrap_transformer_only(module, recurse, **kwargs):
     if recurse:
         return True
     else:
-        return isinstance(module, GPT2Model)
+        return isinstance(module, nn.Transformer)
+
+
+class ModelOutput(OrderedDict):
+    # Partially adapted from the HF transformers lib in order to simulate the behavior
+
+    def to_tuple(self):
+        return tuple(self[k] for k in self.keys())
+
+    def __post_init__(self):
+        class_fields = getattr(self, "__dataclass_fields__")
+
+        for field in class_fields:
+            v = getattr(self, field)
+            if v is not None:
+                self[field] = v
+
+    def __getitem__(self, k):
+        if isinstance(k, str):
+            inner_dict = {k: v for (k, v) in self.items()}
+            return inner_dict[k]
+        else:
+            return self.to_tuple()[k]
+
+
+@dataclass
+class TransformerOutput(ModelOutput):
+    output: torch.FloatTensor = None
+
+
+class TransformerWithCustomOutput(nn.Transformer):  # type: ignore[name-defined]
+    def forward(self, *args, **kwargs):
+        output = super().forward(*args, **kwargs)
+
+        return TransformerOutput(output=output)
+
+
+class DecoderWithLMHead(nn.Module):
+    def __init__(self, d_vocab=100, d_model=16):
+        super().__init__()
+        self.d_vocab = d_vocab
+        self.d_model = d_model
+
+        self.embed_tokens = nn.Embedding(d_vocab, d_model)
+
+        self.transformer = TransformerWithCustomOutput(
+            d_model, num_encoder_layers=2, num_decoder_layers=2, dim_feedforward=64
+        )
+
+        self.output_proj = nn.Linear(d_model, d_vocab)
+
+    def generate_random_sequences(self, seq_len=20, batch_size=2):
+        source_seq = torch.randint(high=self.d_vocab, size=(seq_len, batch_size))
+        target_seq = torch.randint(high=self.d_vocab, size=(seq_len, batch_size))
+
+        return source_seq, target_seq
+
+    def forward(self, source_seq, target_seq):
+        source_embeddings = self.embed_tokens(source_seq)
+        target_embeddings = self.embed_tokens(target_seq)
+
+        output = self.transformer(source_embeddings, target_embeddings)
+
+        # Using integer key here, just like in Huggingface transformer lib
+        return self.output_proj(output[0])
 
 
 class TestHFTransformersAutoWrap(unittest.TestCase):
-
-    pretrained_hf_model = "gpt2"  # "gpt2-large"
-
     def setUp(self) -> None:
         torch.cuda.set_device(0)
 
@@ -37,32 +93,26 @@ class TestHFTransformersAutoWrap(unittest.TestCase):
 
         self.device = torch.device("cuda")
 
-        print("Load tokenizer ...")
-        self.tokenizer = GPT2Tokenizer.from_pretrained(self.pretrained_hf_model)
-
-        print("Load model ...")
-        model_config = GPT2Config.from_pretrained(self.pretrained_hf_model)
-        self.model = GPT2LMHeadModel(model_config)
-
+        print("Build model ...")
+        self.model = DecoderWithLMHead()
         self.model.to(self.device)
 
     def test_auto_wrap_hf_model(self):
 
-        with enable_wrap(wrapper_cls=FSDP, auto_wrap_policy=wrap_gpt2_block_only):
+        with enable_wrap(wrapper_cls=FSDP, auto_wrap_policy=wrap_transformer_only):
             self.model = auto_wrap(self.model)
 
         self.model = FSDP(self.model)
 
-        self.assertTrue(isinstance(self.model.transformer, FSDP), "GPT2Block's should have been wrapped with FSDP")
+        self.assertTrue(isinstance(self.model.transformer, FSDP), "Decoder should have been wrapped with FSDP")
 
-        input_seq = self.tokenizer("Fairscale is great!")["input_ids"]
-        input_seq = torch.LongTensor(input_seq)
+        source_seq, target_seq = self.model.generate_random_sequences()
+        source_seq = source_seq.to(self.device)
+        target_seq = target_seq.to(self.device)
 
-        input_seq = input_seq.to(self.device)
-
-        print(f"Evaluating HF model with input_seq: {input_seq}")
+        print("Evaluating model ...")
         # This should not fail
-        self.model(input_seq)
+        self.model(source_seq, target_seq)
 
 
 if __name__ == "__main__":
