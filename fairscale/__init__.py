cf16261fd5d1e86579025e8c6c6e47e8897e47a8
Anupam Bhatnagar <anupamb@fb.com>
2021-12-15 15:12:46 -0800

    modifying manual workflow

--- fairscale/__init__.py
+++ fairscale/__init__.py
@@ -3,10 +3,6 @@
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
-# Please update the doc version in docs/source/conf.py as well.
-# __version_tuple__ = (0, 4, 3)
-# __version__ = ".".join([str(x) for x in __version_tuple__])
-
 ################################################################################
 # Import most common subpackages
 ################################################################################
