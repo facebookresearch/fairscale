d2d9e32be692fdaf21811688a2a47ce334eb4b42
Anupam Bhatnagar <anupamb@fb.com>
2021-12-22 17:56:20 -0800

    [skip ci] refactoring

--- fairscale/optim/layerwise_gradient_scaler.py
+++ fairscale/optim/layerwise_gradient_scaler.py
@@ -1,8 +1,20 @@
+from collections import defaultdict
+from enum import Enum
 import logging
 from typing import List, Tuple
 
 import torch
 import torch.nn as nn
+
+
+def _refresh_per_optimizer_state():
+    return {"stage": OptState.READY, "found_inf_per_device": {}}
+
+
+class OptState(Enum):
+    READY = 0
+    UNSCALED = 1
+    STEPPED = 2
 
 
 class GradientHelper:
@@ -15,6 +27,8 @@ class GradientHelper:
         scaled_up_grads = list()
         for idx in range(len(inputs)):
             if inputs[idx] is not None:
+                logging.debug("scale up factor is: %s" % self.scale_up_factor)
+                logging.debug("scale down factor is: %s" % self.scale_down_factor)
                 scaled_up_grads.append(inputs[idx].mul(self.scale_up_factor * self.scale_down_factor))
             else:
                 logging.debug("inputs[%d] is None" % idx)
@@ -23,34 +37,95 @@ class GradientHelper:
 
 
 class LayerwiseGradientScaler:
-    def __init__(self) -> None:
+    def __init__(self, growth_factor=2.0 ** 0.001, backoff_factor=0.5 ** 0.5, growth_interval=125):
         self.layer_and_scale_down_factor: List = []
+        self._growth_factor = growth_factor
+        self._backoff_factor = backoff_factor
+        self._growth_interval = growth_interval
 
-    def register_hooks(self, layer, scale_up_factor: float, scale_down_factor: float) -> None:
-        helper = GradientHelper(scale_up_factor, scale_down_factor)
-        layer.register_full_backward_hook(helper.scale_gradients)
+        assert self._growth_factor > 1.0, "The growth factor must be > 1.0."
+        assert self._backoff_factor < 1.0, "The backoff factor must be < 1.0."
 
-    def scale(self, layers_to_scale: List, scaling_factors: List) -> None:
-        for idx in range(len(scaling_factors) - 1, -1, -1):
-            scale_up_inputs_multiplier = scaling_factors[idx - 1] if idx >= 1 else 1
-            scale_down_outputs_multiplier = 1.0 / scaling_factors[idx]
+        # self._per_optimizer_states = defaultdict(_refresh_per_optimizer_state)
 
-            # name and layer tuple
-            _, layer = layers_to_scale[idx][0], layers_to_scale[idx][1]
+    # def _register_hooks(self, layer, scale_up_factor: float, scale_down_factor: float) -> None:
+    #     helper = GradientHelper(scale_up_factor, scale_down_factor)
+    #     layer.register_full_backward_hook(helper.scale_gradients)
+
+    def scale(self, layer_info_list):
+        for idx in range(len(layer_info_list) - 1, -1, -1):
+            name = layer_info_list[idx].name
+            layer = layer_info_list[idx].layer
+
+            scale_down_outputs_multiplier = 1.0 / layer_info_list[idx].scaling_factor
+            scale_up_inputs_multiplier = layer_info_list[idx - 1].scaling_factor if idx >= 1 else 1
 
             self.layer_and_scale_down_factor.append((layer, scale_down_outputs_multiplier))
-            self.register_hooks(layer, scale_up_inputs_multiplier, scale_down_outputs_multiplier)
+            helper = GradientHelper(scale_up_inputs_multiplier, scale_down_outputs_multiplier)
+            layer_handle = layer.register_full_backward_hook(helper.scale_gradients)
 
             logging.info(
                 "layer = %s, input multiplier = %s, output multiplier = %s "
-                % (layers_to_scale[idx][0], scale_up_inputs_multiplier, scale_down_outputs_multiplier)
+                % (name, scale_up_inputs_multiplier, scale_down_outputs_multiplier)
             )
 
     def unscale(self) -> None:
-        # scale down the outputs
+        """
+        If there are no infs/nans in the tensors, then unscale the tensors.
+        """
         for elt in self.layer_and_scale_down_factor:
             layer, factor = elt[0], elt[1]
 
             for param in layer.parameters():
                 if hasattr(param, "grad"):
                     param.grad = torch.mul(param.grad, factor)
+
+    # def _unscale_grads_(self):
+    #     pass
+
+    # def unscale_(self, optimizer):
+    #     """
+    #     checks for infs/nans in the gradient tensors
+    #     """
+    #     optimizer_state = self._per_optimizer_states[id(optimizer)]
+
+    #     if optimizer_state["stage"] is OptState.UNSCALED:
+    #         raise RuntimeError("unscale_() has already been called on this optimizer since the last update().")
+    #     elif optimizer_state["stage"] is OptState.STEPPED:
+    #         raise RuntimeError("unscale_() is being called after step().")
+
+    # def _maybe_optimizer_step(self, optimizer, optimizer_state):
+    #     retval = None
+    #     if not sum(v.items for v in optimizer_state["found_inf_per_device"].value()):
+    #         retval = optimizer.step()
+    #     return retval
+
+    # # unscale the gradients, if gradients don't contain inf/nan then optimizer.step is called,
+    # # otherwise optimizer.step is skipped
+    # def step(self, optimizer):
+    #     """
+    #     1.  Internally invokes ``unscale_(optimizer)`` (unless :meth:`unscale_` was explicitly called for ``optimizer``
+    #         earlier in the iteration).  As part of the :meth:`unscale_`, gradients are checked for infs/NaNs.
+    #     2.  If no inf/NaN gradients are found, invokes ``optimizer.step()`` using the unscaled
+    #         gradients.  Otherwise, ``optimizer.step()`` is skipped to avoid corrupting the params.
+    #     """
+
+    #     # self._check_scale_growth_tracker("step")
+    #     optimizer_state = self._per_optimizer_states[id(optimizer)]
+
+    #     if optimizer_state["stage"] is OptState.STEPPED:
+    #         raise RuntimeError("step() has already been called since the last update().")
+
+    #     if optimizer_state["stage"] is OptState.READY:
+    #         self.unscale_(optimizer)
+
+    #     assert len(optimizer_state["found_inf_per_device"]) > 0, "No inf checks were recorded for this optimizer."
+
+    #     retval = self._maybe_optimizer_step(optimizer, optimizer_state)
+    #     optimizer_state["stage"] = OptState.STEPPED
+    #     return retval
+
+    # # this function should update the scaling factor for each layer
+    # # clip the scale to the range [2 ** 7, 2 ** 24]
+    # def update():
+    #     pass
--- tests/optim/test_layerwise_gradient_scaler.py
+++ tests/optim/test_layerwise_gradient_scaler.py
@@ -10,8 +10,6 @@ import torchvision.transforms as transforms
 
 from fairscale.optim.layerwise_gradient_scaler import LayerwiseGradientScaler
 
-# torch.backends.cudnn.benchmark = False
-
 
 # Test 1: a simple feed forward network
 class Feedforward(torch.nn.Module):
@@ -148,7 +146,7 @@ class SimpleConvNet(nn.Module):
         self.fc2 = nn.Linear(120, 84)
         self.relu4 = nn.ReLU()
         self.fc3 = nn.Linear(84, 10)
-        self.last_layer = nn.Identity()
+        self.identity = nn.Identity()
 
     def forward(self, x):
         out = self.conv1(x)
@@ -163,7 +161,7 @@ class SimpleConvNet(nn.Module):
         out = self.fc2(out)
         out = self.relu4(out)
         out = self.fc3(out)
-        out = self.last_layer(out)
+        out = self.identity(out)
         return out
 
 
@@ -172,7 +170,7 @@ def load_data_for_vision():
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
     train_ds = torchvision.datasets.CIFAR10(root="./data", train=True, download=True, transform=transform)
-    train_ds_loader = torch.utils.data.DataLoader(train_ds, batch_size=32, shuffle=False, num_workers=2)
+    train_ds_loader = torch.utils.data.DataLoader(train_ds, batch_size=128, shuffle=False, num_workers=2)
 
     # test_ds = torchvision.datasets.CIFAR10(root="./data", train=False, download=True, transform=transform)
     # test_ds_loader = torch.utils.data.DataLoader(test_ds, batch_size=32, shuffle=False, num_workers=2)
@@ -182,17 +180,26 @@ def load_data_for_vision():
     return train_ds_loader
 
 
-def vision_training(model: SimpleConvNet, scaler: LayerwiseGradientScaler = None):
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+class LayerInfo:
+    def __init__(self, name, layer, scaling_factor, growth_tracker=0):
+        self.name = name
+        self.layer = layer
+        self.scaling_factor = scaling_factor
+        self.growth_tracker = growth_tracker
 
+
+def generic_vision_training(model: SimpleConvNet, scaler: LayerwiseGradientScaler = None):
     train_ds_loader = load_data_for_vision()
-    num_epochs = 1
 
     if torch.cuda.is_available():
         model.cuda()
 
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    num_epochs = 2
     model.train()
+
     for _ in range(num_epochs):
         for img, lbl in train_ds_loader:
             img = img.cuda()
@@ -208,7 +215,47 @@ def vision_training(model: SimpleConvNet, scaler: LayerwiseGradientScaler = None
                 scaler.unscale()
 
             optimizer.step()
+    return model
 
+
+def scaled_vision_training(model: SimpleConvNet):
+    layers_to_scale = set(["conv1", "fc2", "fc3"])
+    layer_info_list = list()
+    default_scaling_factor = 2 ** 13
+
+    for name, layer in model.named_modules():
+        if name != "":
+            if name not in layers_to_scale:
+                layer_info_list.append(LayerInfo(name, layer, 1.0))
+            else:
+                layer_info_list.append(LayerInfo(name, layer, default_scaling_factor))
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    if torch.cuda.is_available():
+        model.cuda()
+
+    layerwise_scaler = LayerwiseGradientScaler()
+    layerwise_scaler.scale(layer_info_list)
+
+    train_ds_loader = load_data_for_vision()
+    model.train()
+
+    for _ in range(2):
+        for img, lbl in train_ds_loader:
+            img = img.cuda()
+            lbl = lbl.cuda()
+
+            optimizer.zero_grad()
+            predict = model(img)
+
+            loss = loss_fn(predict, lbl)
+            loss.backward()
+
+            layerwise_scaler.unscale()
+
+            optimizer.step()
     return model
 
 
@@ -223,37 +270,8 @@ def test_vision_model() -> None:
     m1 = SimpleConvNet()
     m2 = SimpleConvNet()
 
-    layers_to_scale = []
-    scaling_factors = [32, 1]
-
-    for name, layer in m2.named_modules():
-        # if isinstance(layer, nn.Conv2d):
-        # print(name, layer)
-        if name in ["conv2", "relu2"]:
-            layers_to_scale.append([name, layer])
-
-    print(layers_to_scale)
-    assert len(scaling_factors) == len(layers_to_scale)
-    layerwise_scaler = LayerwiseGradientScaler()
-    layerwise_scaler.scale(layers_to_scale, scaling_factors)
-
-    vision_model = vision_training(m1)
-    scaled_vision_model = vision_training(m2, layerwise_scaler)
-
-    # assert torch.equal(vision_model.conv1.weight.grad, scaled_vision_model.conv1.weight.grad)
-    # assert torch.equal(vision_model.conv1.bias.grad, scaled_vision_model.conv1.bias.grad)
-
-    # assert torch.equal(vision_model.conv2.weight.grad, scaled_vision_model.conv2.weight.grad)
-    # assert torch.equal(vision_model.conv2.bias.grad, scaled_vision_model.conv2.bias.grad)
-
-    # assert torch.equal(vision_model.fc1.weight.grad, scaled_vision_model.fc1.weight.grad)
-    # assert torch.equal(vision_model.fc1.bias.grad, scaled_vision_model.fc1.bias.grad)
-
-    # assert torch.equal(vision_model.fc2.weight.grad, scaled_vision_model.fc2.weight.grad)
-    # assert torch.equal(vision_model.fc2.bias.grad, scaled_vision_model.fc2.bias.grad)
-
-    # assert torch.equal(vision_model.fc3.weight.grad, scaled_vision_model.fc3.weight.grad)
-    # assert torch.equal(vision_model.fc3.bias.grad, scaled_vision_model.fc3.bias.grad)
+    vision_model = generic_vision_training(m1)
+    scaled_vision_model = scaled_vision_training(m2)
 
     def get_params_with_grad(trained_model):
         result = []
@@ -271,8 +289,18 @@ def test_vision_model() -> None:
     """
     IMPORTANT: in the shell, export CUBLAS_WORKSPACE_CONFIG=:4096:8 to get rid of randomness from different sources.
 
-    scaling and unscaling should be done taking the device into account. at present the scaling
-    and unscaling functions do not take the device into account.
+    - scaling and unscaling should be done taking the number of gpus into account. at present the scaling
+    and unscaling functions do not take the number of gpus into account. In particular, default_scaling_factor should be a function of the number of gpus.
+    - run vision example using GPUs DONE
+    - initialize a default scaling factor to 1 for each layer. DONE
+    - for the layers specified by the user, modify the scaling factor to a value larger than 1. DONE
+    - set default growth interval, growth factor, backoff factor for the scale
+    - allow the user to specify the initial value of the scaling factor and make the growth interval, growth factor, backoff factor configurable by the user.
+    - ensure that training is happening on gpu instead of cpu
+    - test code in multi gpu setting. current vision example uses a single gpu
+    - there will be multiple optimizers (one per gpu) in the multi gpu setting,
 
-    Run vision example using GPUs
+    open questions:
+    - if there is an inf encountered in the gradient of a layer should the weight update be skipped for that layer or all the layers in that step?
+
     """
