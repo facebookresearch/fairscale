b4689459ae59512f7004cd75895f5db4b19ab858
Anjali Sridhar <anj@devfair0443.h2.fair>
2022-02-07 15:30:45 -0800

    merge main

--- benchmarks/datasets/wikitext2_data.py
+++ benchmarks/datasets/wikitext2_data.py
@@ -12,6 +12,7 @@ import tempfile
 import torch
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
+
 import torchtext
 from torchtext.data.utils import get_tokenizer
 from torchtext.utils import download_from_url, extract_archive
