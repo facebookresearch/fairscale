dcb206d85b74aca84e2f28e502d1ca84de5ac99c
Anjali Sridhar <anj@devfair0443.h2.fair>
2022-02-07 15:25:48 -0800

    lint errors

--- benchmarks/golden_configs/lm_wikitext2.py
+++ benchmarks/golden_configs/lm_wikitext2.py
@@ -66,6 +66,42 @@ class Offload_Sequential:
         }
 
 
+class FSDP:
+    def get_model_config():
+        return {
+            "vocab_size": 10000,
+            "ninp": 2048,  # embedding dimension
+            "nhid": 2048,  # the dimension of the feedforward network model in nn.TransformerEncoder
+            "nhead": 32,  # the number of heads in the multiheadattention models
+            "dropout": 0,
+            "initrange": 0.1,
+            "scaler": GradScaler(),
+            "clip_value": 0.05,
+            "num_decoder_layers": 10,
+            "seq_len": 32,
+        }
+
+    def get_benchmark_config():
+
+        return {
+            "epochs": 1,
+            "lr": 0.001,  # learning rate
+            "batch_size": 8,
+            "criterion": nn.CrossEntropyLoss(),
+        }
+
+    def get_golden_real_stats():
+        return {
+            "avg_wps": 703.778,
+            "std_dev_wps": 5.732,
+            "peak_mem_usage": [2320996352, 1396742144, 1396742144, 2340010496],
+        }
+
+    def get_golden_synthetic_stats():
+        # TODO(anj-s): Add support for synthetic regression benchmarks
+        raise NotImplementedError("Synthetic data benchmarks are not supported.")
+
+
 class Pipe:
     def get_model_config():
         return {
