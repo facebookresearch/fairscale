b6c5474d203bb223c86814d10ab86f820f2ba846
Anjali Sridhar <anj@devfair0443.h2.fair>
2022-02-07 16:36:54 -0800

    lint error

--- benchmarks/datasets/wikitext2_data.py
+++ benchmarks/datasets/wikitext2_data.py
@@ -12,7 +12,6 @@ import tempfile
 import torch
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
-
 import torchtext
 from torchtext.data.utils import get_tokenizer
 from torchtext.utils import download_from_url, extract_archive
