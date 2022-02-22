f9a125db0d3f4a724a02f1065a65af45353c4241
anj-s <32556631+anj-s@users.noreply.github.com>
2022-02-22 11:57:13 -0800

    [benchmarks] Add benchmarks for FSDP (#765)
    * add benchmarks for fsdp
    
    * fix lint errors
    
    * clean up
    
    * clean up unused flags
    
    * add the benchmarks
    
    * remove unused args
    
    * fix lint errors
    
    * fix lint errors
    
    * update command line
    
    * add support for multiple devices
    
    * try full fp16 mode
    
    * try full fp16 mode
    
    * lint errors
    
    * merge main
    
    * lint errors
    
    * lint errors
    
    * lint error
    
    * update intersphinx mapping for numpy
    
    * update intersphinx mapping for numpy
    
    * skip test
    
    * added golden configs
    
    * use synthetic benchmarks
    
    * fix fn name
    
    * fix cuda device id
    
    * fix verify
    
    * lint fix

--- benchmarks/golden_configs/lm_wikitext2.py
+++ benchmarks/golden_configs/lm_wikitext2.py
@@ -66,6 +66,41 @@ class Offload_Sequential:
         }
 
 
+class FSDP:
+    def get_model_config():
+        return {
+            "vocab_size": 10000,
+            "ninp": 2048,  # embedding dimension
+            "nhid": 2048,  # the dimension of the feedforward network model in nn.TransformerEncoder
+            "nhead": 32,  # the number of heads in the multiheadattention models
+            "dropout": 0,
+            "initrange": 0.1,
+            "scaler": GradScaler(),
+            "clip_value": 0.05,
+            "num_decoder_layers": 10,
+            "seq_len": 32,
+        }
+
+    def get_benchmark_config():
+
+        return {
+            "epochs": 1,
+            "lr": 0.001,  # learning rate
+            "batch_size": 8,
+            "criterion": nn.CrossEntropyLoss(),
+        }
+
+    def get_golden_real_stats():
+        raise NotImplementedError("Synthetic data benchmarks are not supported.")
+
+    def get_golden_synthetic_stats():
+        return {
+            "avg_wps": 486.303,
+            "std_dev_wps": 71.307,
+            "peak_mem_usage": [5.5055 * 2 ** 30, 5.5055 * 2 ** 30, 5.5055 * 2 ** 30, 5.5055 * 2 ** 30],
+        }
+
+
 class Pipe:
     def get_model_config():
         return {
