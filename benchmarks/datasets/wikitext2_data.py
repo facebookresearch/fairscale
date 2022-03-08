135fa7c29d8dcccbc992d1b2122e0a51a5cd2361
Min Xu <min.xu.public@gmail.com>
2022-03-08 11:04:19 -0800

    copyright headers

--- benchmarks/datasets/wikitext2_data.py
+++ benchmarks/datasets/wikitext2_data.py
@@ -1,4 +1,4 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
 #
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
