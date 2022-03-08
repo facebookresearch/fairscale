8fa26ae4f89c165c452b1f55b82045c1b8984363
Min Xu <24926999+min-xu-ai@users.noreply.github.com>
2022-03-08 13:14:18 -0800

    [chore] Fix copyright headers & fixed issue with mypy & NumPy versions in pre-commit (#951)
    * copyright headers
    
    * isort and pyproject.toml
    
    * precommit and requirement for isort-seed-config
    
    * mypy
    
    * dummy change
    
    * numpy version for pre-commit
    
    * fix mypy issue caused by numpy
    
    Co-authored-by: Min Xu <min.xu.public@gmail.com>

--- benchmarks/datasets/wikitext2_data.py
+++ benchmarks/datasets/wikitext2_data.py
@@ -1,14 +1,14 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
 #
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
 from collections import namedtuple
-from distutils.version import LooseVersion
 import io
 import operator
 import tempfile
 
+from distutils.version import LooseVersion
 import torch
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
