0bcf4dae3035f57852e4a66271a9e2d2c58d2610
Riyasat Ohib <riyasat.ohib@gmail.com>
2022-05-13 16:29:43 -0700

    [refactor] Modified collections to collections.abc

--- fairscale/optim/grad_scaler.py
+++ fairscale/optim/grad_scaler.py
@@ -3,7 +3,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from collections import abc, defaultdict
+import collections.abc as abc
+from collections import defaultdict
+
 from enum import Enum
 import logging
 from typing import Any, Dict, List, Optional, Union
--- fairscale/utils/params.py
+++ fairscale/utils/params.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
-from collections import abc
+import collections.abc as abc
 from math import inf
 from typing import Any, Callable, Dict, List, Optional
 
