317b32accf57a47abb4e01ba370ba04e8ff34234
Min Xu <min.xu.public@gmail.com>
2022-05-13 16:44:48 -0700

    Revert "[refactor] Modified collections to collections.abc"
    This reverts commit 0bcf4dae3035f57852e4a66271a9e2d2c58d2610.

--- fairscale/optim/grad_scaler.py
+++ fairscale/optim/grad_scaler.py
@@ -3,9 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import collections.abc as abc
-from collections import defaultdict
-
+from collections import abc, defaultdict
 from enum import Enum
 import logging
 from typing import Any, Dict, List, Optional, Union
