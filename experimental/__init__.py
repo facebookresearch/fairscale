3f898627828b7a797feefd51fde399690deb8610
Riyasat Ohib <riyasat.ohib@gmail.com>
2022-05-23 19:24:45 -0700

    [feat] adding experimental wgit

--- experimental/__init__.py
+++ experimental/__init__.py
@@ -2,7 +2,3 @@
 #
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
-
-from .cli import main
-
-__version__ = "0.0.1"
--- experimental/tests/cli/test_cli.py
+++
@@ -1,8 +0,0 @@
-import wgit.cli as cli
-
-
-def test_cli_init(capsys):
-    cli.main(["--init"])
-    out, err = capsys.readouterr()
-    assert out == "Hello World, Wgit has been initialized!\n"
-    assert err == ""
