b27ac3baa125cb2cad2fb7c5bb30bae2a3cb7c7b
Riyasat Ohib <riyasat.ohib@gmail.com>
2022-06-24 13:56:20 -0700

    [Fix] Restructure for wgit availability as a package

--- fairscale/experimental/wgit/__init__.py
+++ fairscale/experimental/wgit/__init__.py
@@ -3,7 +3,10 @@
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
+from typing import List
+
 from .repo import Repo
 from .version import __version_tuple__
 
 __version__ = ".".join([str(x) for x in __version_tuple__])
+__all__: List[str] = []
