8be747073be11000ab28658427d899a9909eeb7d
Min Xu <min.xu.public@gmail.com>
2022-08-06 14:40:44 -0700

    added a profiling class

---
+++ fairscale/experimental/wgit/signal_sparsity_profiling.py
@@ -0,0 +1,60 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import List
+
+import torch
+from torch import Tensor
+
+class EnergyConcentrationProfile:
+    """ Compute "energy" concentration level for a tensor
+
+    Args:
+        dim (int):
+            The dimension to measure.
+        top_k_percents (List[float]):
+            List of percentage values. For each value, the `measure`
+            function will compute and return the percentage of "energy"
+            concentrated on that top-K percent of values in the dimension
+            to measure.
+    """
+    def __init__(self, dim: int, top_k_percents: List[float]) -> None:
+        assert isinstance(dim, int)
+        self.dim = dim
+        self.percents = []
+        last_p = 0.
+        for p in top_k_percents:
+            assert isinstance(p, float)
+            assert p > 0, p
+            assert p > last_p, f"p {p} should be larger than last_p {last_p}"
+            self.percents.append(p)
+            last_p = p
+
+    def measure(self, in_tensor: Tensor) -> List[Tensor]:
+        """ Compute the return the results
+
+            Note, we want this function to be nonblocking and async.
+
+        Returns:
+            (List[Tensor])
+                List of tensors. Each tensor is a singleton float
+                that contains the energy measure for that top_k_percent.
+        """
+        assert in_tensor.is_floating_point(), in_tensor.dtype
+        assert self.dim < len(in_tensor.shape), f"tensor shape {in_tensor.shape} not compatible with dim {self.dim}"
+        dim_size = in_tensor.shape[self.dim]
+        abs_tensor = in_tensor.abs()
+        full_energy = abs_tensor.sum()
+        return_tensors = []
+        for p in self.percents:
+            k = max(1, int(p * dim_size))
+            abs_top_k_values, _ = abs_tensor.topk(k, dim=self.dim)
+            return_tensors.append(abs_top_k_values.sum() / full_energy)
+        return return_tensors
+
+    def measure_fft(self, in_tensor: Tensor) -> List[Tensor]:
+        """Like measure, but do it in FFT frequency domain."""
+        # XXX: remove type ignore once the fft_fix branch is merged
+        return self.measure(torch.fft.fft(in_tensor, dim=self.dim).real)  # type: ignore
