[fix] SDP syncing buffers during gradient accumulation (#1075)
- Fixes from Benjamin.

Original commit msg:
  - Fixes #1041. I just had a minute or two, hoping that it's enough :)

Co-authored-by: Min Xu <min.xu.public@gmail.com>

fairscale/nn/data_parallel/sharded_ddp.py
@@ -218,7 +218,7 @@ class ShardedDataParallel(nn.Module):
             if needs_setup:
                 self.refresh_trainable()
 
-            if self._enable_broadcast_buffers:
+            if self._enable_broadcast_buffers and not self._should_accumulate_grads:
                 # NCCL communications are on a different stream, needs to be blocking
                 # for the subsequent FW to be correct
                 self.sync_buffers(blocking=True)