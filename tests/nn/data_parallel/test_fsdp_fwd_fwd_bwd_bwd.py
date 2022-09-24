6b5650757d3e286c6b131634dcb4080f1a9b2418
Min Xu <min.xu.public@gmail.com>
2022-09-24 08:03:55 -0700

    add new test

---
+++ tests/nn/data_parallel/test_fsdp_fwd_fwd_bwd_bwd.py
@@ -0,0 +1,68 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torch
+import torch.nn as nn
+
+from fairscale.fair_dev.testing.testing import skip_if_single_gpu, temp_files_ctx
+from fairscale.nn import enable_wrap, wrap
+from fairscale.nn.data_parallel import FullyShardedDataParallel
+
+
+class FFN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(10, 10)
+        self.fc2 = nn.Linear(10, 10)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
+
+
+def main(rank, sync_file):
+    torch.manual_seed(0)
+    torch.cuda.manual_seed(0)
+    torch.cuda.set_device(rank)
+    torch.distributed.init_process_group(
+        backend="nccl",
+        init_method=f"file://{sync_file}",
+        world_size=2,
+        rank=rank,
+    )
+    ffn = FFN()
+
+    with enable_wrap(wrapper_cls=FullyShardedDataParallel):
+        model = wrap(
+            ffn.half(),
+            process_group=torch.distributed.new_group(),
+            flatten_parameters=True,
+            compute_dtype=torch.float16,
+        )
+
+    model = model.train()
+
+    x1 = torch.rand((10, 10)).cuda().half()
+    out1 = model(x1)
+    loss1 = out1.sum()
+
+    x2 = torch.rand((10, 10)).cuda().half()
+    out2 = model(x2)
+    loss2 = out2.sum()
+
+    with model.no_sync():
+        loss1.backward()
+        loss2.backward()
+
+
+@skip_if_single_gpu
+def test_fwd_fwd_bwd_bwd():
+    with temp_files_ctx(num=1) as temp_files:
+        torch.multiprocessing.spawn(
+            fn=main,
+            nprocs=2,
+            args=(temp_files[0],),
+            join=True,
+        )
