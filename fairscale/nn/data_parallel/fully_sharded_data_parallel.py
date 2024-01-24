8ec7c1df8ecb1e963fe40960bbe2ebe9cd8f3d07
ngoyal2707 <ngoyal2707@users.noreply.github.com>
2024-01-24 15:49:34 -0500

    added option for no PG validation for faster init (#1161)
    Co-authored-by: Naman Goyal <naman@fb.com>

--- fairscale/nn/data_parallel/fully_sharded_data_parallel.py
+++ fairscale/nn/data_parallel/fully_sharded_data_parallel.py
@@ -371,6 +371,7 @@ class FullyShardedDataParallel(nn.Module):
         limit_all_gather_events: bool = False,
         limit_reduce_scatter_events: bool = False,
         cast_input: bool = True,
+        should_validate_process_group: bool = True,
     ):
         try:
             import torch._C
@@ -455,7 +456,7 @@ class FullyShardedDataParallel(nn.Module):
             raise ValueError(f"offload type: '{offload_config.offload_type}' requires flatten_parameters=True")
 
         # skip validation if the process group was created above
-        if process_group:
+        if process_group and should_validate_process_group:
             validate_process_group(self.compute_device, self.process_group)
 
         # enable pytorch sync_bn just in case model contains sync_bn layers.
